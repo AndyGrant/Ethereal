05506f808b812ae38ca1080fa3b62c1910e0e67f
AndyGrant <Andrew@grantnet.us>
2015-04-08 00:21:24 -0400

    Added profiler

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ javac -d ../_Classes JChess.java
 cd ../
 
 cd _Core
-gcc -o3 _Scripts/Engine.c _Scripts/Moves.c _Scripts/ChessAI.c _Scripts/TranspositionTable.c engineFromJava.c -o engineFromJava.exe
+gcc -p -pg -o3 _Scripts/Engine.c _Scripts/Moves.c _Scripts/ChessAI.c _Scripts/TranspositionTable.c engineFromJava.c -o engineFromJava.exe
 cd ../
 
 
