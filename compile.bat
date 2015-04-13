38a85ac7bc7a2b14a4fa1c58fb65c00ff1bd54b6
AndyGrant <andrew@grantnet.us>
2015-04-13 10:16:12 -0400

    redirected output stream, removed profiler

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ javac -d ../_Classes JChess.java
 cd ../
 
 cd _Core
-gcc -p -pg -o3 _Scripts/Engine.c _Scripts/Moves.c _Scripts/ChessAI.c _Scripts/BinTable.c engineFromJava.c -o engineFromJava.exe
+gcc -o3 _Scripts/Engine.c _Scripts/Moves.c _Scripts/ChessAI.c _Scripts/BinTable.c engineFromJava.c -o engineFromJava.exe
 cd ../
 
 
