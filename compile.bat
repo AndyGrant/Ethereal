eb2dd31026ce1d3b2ce41de93b45d9f4f661afa9
AndyGrant <Andrew@grantnet.us>
2015-04-14 16:06:51 -0400

    Removed old compiler

--- compile.bat
+++
@@ -1,9 +0,0 @@
-cd _Scripts
-javac -d ../_Classes JChess.java
-cd ../
-
-cd _Core
-gcc -o3 _Scripts/Engine.c _Scripts/Moves.c _Scripts/ChessAI.c _Scripts/BinTable.c engineFromJava.c -o engineFromJava.exe
-cd ../
-
-
