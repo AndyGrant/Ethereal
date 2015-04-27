cfe495d74926c73c7482175dd72090b09205ad21
AndyGrant <andrew@grantnet.us>
2015-04-27 10:54:56 -0400

    Removed ignored files

--- C_compile.bat
+++
@@ -1,3 +0,0 @@
-cd _Core
-gcc -o3 _Scripts/Engine.c _Scripts/Moves.c _Scripts/ChessAI.c _Scripts/BinTable.c engineFromJava.c -o engineFromJava.exe
-cd ../
--- JAVA_compile.bat
+++
@@ -1,3 +0,0 @@
-cd _Scripts
-javac -d ../_Classes JChess.java
-cd ../
