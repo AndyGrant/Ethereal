03d79c7284e0b2d090f0da575671c0d4e768a413
AndyGrant <andrew@grantnet.us>
2015-04-30 12:23:05 -0400

    Moved some args to globals

--- _Core/_Scripts/ChessAI.h
+++ _Core/_Scripts/ChessAI.h
@@ -5,16 +5,16 @@
 #include "BinTable.h"
 
 int findBestMoveIndex(Board * board, int * last_move, int turn);
-int alphaBetaPrune(BinaryTable * table , Board *b, int turn, int * move, int depth, int alpha, int beta, int evaluatingPlayer);
+int alphaBetaPrune(int turn, int * move, int depth, int alpha, int beta, int evaluatingPlayer);
 
-int evaluateBoard(Board *b, int player, int * lastMove);
-int evaluatePosition(Board *b, int player);
-int evaluateMaterial(Board *b, int player);
-int evaluateMoves(Board *b, int player, int * lastMove);
+int evaluateBoard(int player, int * lastMove);
+int evaluatePosition(int player);
+int evaluateMaterial(int player);
+int evaluateMoves(int player, int * lastMove);
 
-int * weakHeuristic(Board *board, int size, int * moves);
+int * weakHeuristic(int size, int * moves);
 
-int endAISearch(int reached, int size, int * values, int * moves, int * unsorted, BinaryTable * table);
+int endAISearch(int reached, int size, int * values, int * moves, int * unsorted);
 int * sortMoves(int * values, int * moves, int size);
 
 #endif
