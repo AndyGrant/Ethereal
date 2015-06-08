6ae829a1a45e2a4eb9f0c274c64294344d0d25d7
AndyGrant <Andrew@grantnet.us>
2015-06-08 00:08:03 -0400

    reverted all changes

--- _Core/_Scripts/BinTable.h
+++ _Core/_Scripts/BinTable.h
@@ -5,41 +5,30 @@
 
 extern int KEY_SIZE;
 extern int LAST_KEY;
-extern int LOWERBOUND;
-extern int UPPERBOUND;
-extern int EXACT;
 
 typedef struct BinaryTable{
-	struct BinaryTree ** trees;
-	int elements;
-	int size;
-} BinaryTable;
-
-typedef struct BinaryTree{
 	struct Node * root;
 	int elements;
-} BinaryTree;
+	
+} BinaryTable;
 
 typedef struct Node{
 	struct Node * left;
 	struct Node * right;
 	int value;
 	int * key;
-	int depth;
-	int type;
+	
 } Node;
 
 
-BinaryTable * createTable(int size);
-BinaryTree * createTree();
-Node * createNode(int value, int * key, int depth, int type);
+BinaryTable * createTable();
+Node * createNode(int value, int * key);
 
 void destroyTable(BinaryTable * table);
-void destroyTree(BinaryTree * tree);
 void destroyNode(Node * node);
 
-void insertElement(BinaryTable * table, int value, int * key, int depth, int type);
-Node * getElement(BinaryTable * table, int depth, int * key);
+void insertElement(BinaryTable * table, int value, int * key);
+Node * getElement(BinaryTable * table, int * key);
 
 int * encodeBoard(Board * b, int enpass);
 
