bcf23c5b99d7d04252afd0e595180a28021aadf9
AndyGrant <Andrew@grantnet.us>
2015-06-12 03:36:09 -0400

    Tranpsosions now have depth levels and types. Indicating wether they are upper or lower bounds, or exact values on certain transpositions. All depth levels have access to the Transposition Table and can use the Transpositions to modify their search bounds

--- _Core/_Scripts/BinTable.h
+++ _Core/_Scripts/BinTable.h
@@ -5,6 +5,9 @@
 
 extern int KEY_SIZE;
 extern int LAST_KEY;
+extern int EXACT;
+extern int LOWERBOUND;
+extern int UPPERBOUND;
 
 typedef struct BinaryTable{
 	struct Node * root;
@@ -17,17 +20,19 @@ typedef struct Node{
 	struct Node * right;
 	int value;
 	int * key;
+	int depth;
+	int type;
 	
 } Node;
 
 
 BinaryTable * createTable();
-Node * createNode(int value, int * key);
+Node * createNode(int value, int * key, int depth, int type);
 
 void destroyTable(BinaryTable * table);
 void destroyNode(Node * node);
 
-void insertElement(BinaryTable * table, int value, int * key);
+void insertElement(BinaryTable * table, int value, int * key, int depth, int type);
 Node * getElement(BinaryTable * table, int * key);
 
 int * encodeBoard(Board * b, int enpass, int turn);
