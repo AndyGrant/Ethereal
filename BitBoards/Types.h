b424cf43d32af03c7baf1dcd55e0b8adaf8a5490
AndyGrant <Andrew@grantnet.us>
2015-07-19 22:53:24 -0400

    .

--- BitBoards/Types.h
+++ BitBoards/Types.h
@@ -12,6 +12,7 @@ typedef struct Board {
 	BitBoard Knights;
 	BitBoard Bishops;
 	BitBoard Pawns;
+	BitBoard * Pieces[6];
 	
 	int Turn;
 	int Enpass;
@@ -49,12 +50,20 @@ typedef struct Move {
 	int Flags;
 	
 	/* Flags
-		0-64 -> pawn double move origin tile
-		65 	-> invalidates king-side castle
-		66   -> invalidates queen-side castle
-		67 	-> Enpass
-		68 	-> Promotion
-		69	-> Castle
+	
+		-1		-> No Flags
+		0-64 	-> Pawn double move from tile X
+		65		-> Promote to Queen
+		66		-> Promote to Rook
+		67		-> Promote to Knight
+		68		-> Promote to Bishop
+		70		-> Invalidate Left Castle
+		71		-> Invalidate Right Castle
+		72		-> Invalidate both Castle		
+		80		-> Left Castle
+		81		-> Right Castle
+		90		-> Enpass
+		
 	*/
 	
 } Move;
