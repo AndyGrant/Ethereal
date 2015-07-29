bd77ae715406175db96c8497cf9edf2f4dfcd2d5
AndyGrant <Andrew@grantnet.us>
2015-07-29 16:46:01 -0400

    .

---
+++ BitBoards/Moves.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Types.h"
+#include "Board.h"
+#include "Engine.h"
+#include "Moves.h"
+
+
---
+++ BitBoards/Moves.h
@@ -0,0 +1,10 @@
+#ifndef Moves
+#define Moves
+
+#include "board.h"
+#include "types.h"
+#include "engine.h"
+
+
+
+#endif
