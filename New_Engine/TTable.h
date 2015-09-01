1e2e999bbd5523e8b80610113f56ad6ea0ebec9f
AndrewGrant <andrew@grantnet.us>
2015-09-01 11:23:53 -0400

    .

--- New_Engine/TTable.h
+++ New_Engine/TTable.h
@@ -1,1 +1,41 @@
+#ifndef TTABLE_HEADER
+#define TTABLE_HEADER
 
+#define EXACT = 1
+#define LOWERBOUND = 2
+#define UPPERBOUND = 3
+
+#define NUM_BUCKETS = 4096
+#define BUCKET_SIZE = 512
+
+typedef struct Node{
+	int * key;
+	int depth;
+	int type;
+	int turn;
+} Node;
+
+typedef struct Bucket{
+	int max;
+	int size;
+	Node * nodes;
+} Bucket;
+
+typedef struct TTable{
+	int size;
+	Bucket * buckets;
+} TTable;
+
+Node * createNode(int * key, int depth, int type, int turn);
+Bucket * createBucket();
+TTable * createTTable();
+
+void getNodeType(Node * node, int alpha, int beta);
+Node * getNode(TTable * table, int hash, int * key);
+void storeNode(TTable * table, int hash, int * key, int depth, int type, int turn);
+
+int createKey(Board * board);
+int createHash(int * key);
+ 
+
+#endif
