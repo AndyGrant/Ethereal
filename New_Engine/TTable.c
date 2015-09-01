8ac400ca3f535ae7436a62090e64a3a3052b1934
AndrewGrant <andrew@grantnet.us>
2015-09-01 12:41:28 -0400

    .

--- New_Engine/TTable.c
+++ New_Engine/TTable.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Engine.c"
+#include "TTable.h"
+
+
+Node * createNode(int * key, int depth, int type, int turn){
+	
+}
+
+Bucket * createbucket(){
+	
+}
+
 
