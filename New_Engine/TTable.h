34db2f45998d20f23d4a465a27753b8e34cbb939
AndrewGrant <andrew@grantnet.us>
2015-09-03 08:07:41 -0400

    .

--- New_Engine/TTable.h
+++ New_Engine/TTable.h
@@ -1,14 +1,16 @@
 #ifndef TTABLE_HEADER
 #define TTABLE_HEADER
 
-#include "Engine.c"
+#include "Engine.h"
 
-#define EXACT = 1
-#define LOWERBOUND = 2
-#define UPPERBOUND = 3
+#define EXACT 1
+#define LOWERBOUND 2
+#define UPPERBOUND 3
 
-#define NUM_BUCKETS = 4096
-#define BUCKET_SIZE = 512
+#define NUM_BUCKETS 16000
+#define BUCKET_SIZE 512
+
+#define KEY_SIZE 9
 
 typedef struct Node{
 	int * key;
@@ -20,12 +22,12 @@ typedef struct Node{
 typedef struct Bucket{
 	int max;
 	int size;
-	Node * nodes;
+	Node ** nodes;
 } Bucket;
 
 typedef struct TTable{
 	int size;
-	Bucket * buckets;
+	Bucket * buckets[NUM_BUCKETS];
 } TTable;
 
 Node * createNode(int * key, int depth, int type, int turn);
@@ -34,9 +36,9 @@ TTable * createTTable();
 
 void setNodeType(Node * node, int alpha, int beta, int value);
 Node * getNode(TTable * table, int hash, int * key);
-void storeNode(TTable * table, int hash, int * key, int depth, int type, int turn);
+void storeNode(TTable * table, int hash, Node * node);
 
-int createKey(Board * board);
+int * createKey(Board * board);
 int createHash(int * key);
  
 #endif
