ba60f9369d5970f1d9e52dc5f46ebe2ca0b5025c
AndyGrant <Andrew@grantnet.us>
2015-09-05 18:09:49 -0400

    .

--- New_Engine/Search.c
+++ New_Engine/Search.c
@@ -9,24 +9,28 @@
 #include "TTable.h"
 
 int MATERIAL_VALUES[6] = {100,300,300,500,1000,10000};
+int CAPTURE_VALUES[6] = {1,3,3,6,9,1};
 
 TTable * TABLE;
 
 time_t START_TIME;
 time_t END_TIME;
+
+
 int START_DEPTH = 2;
 int MAX_DEPTH = 10;
 int DELTA_DEPTH = 2;
 int END_DEPTH = 10;
 
 int TOTAL_BOARDS_SEARCHED = 0;
+int TOTAL_MOVES_FOUND = 0;
 
 int getBestMoveIndex(Board * board, int turn){
 
 	START_TIME = time(NULL);
 	END_TIME = START_TIME + 9;
 	
-	//TABLE = createTTable();
+	TABLE = createTTable();
 	
 	int size = 0;
 	int * moves = getAllMoves(board,turn,&size);
@@ -69,33 +73,88 @@ int getBestMoveIndex(Board * board, int turn){
 }
 
 int endSearch(int index, int size, int * values, int * sorted, int * unsorted){
+	printf("==========================================\n");
+	printf("Total Boards Searched \t: %d\n",TOTAL_BOARDS_SEARCHED);
+	printf("Total Moves Found \t: %d\n",TOTAL_MOVES_FOUND);
 	return 0;
 }
 
 void valueSort(int * values, int * moves, int size){
-	return;
+	int i,j;
+	int a[5], b[5];
+	for(i = 0; i < size; i++){
+		for(j = i + 1; j < size; j++){
+			if (values[j] > values[i]){
+				memcpy(a,moves+i*5,sizeof(int) * 5);
+				memcpy(b,moves+j*5,sizeof(int) * 5);
+				memcpy(moves+j*5,a,sizeof(int) * 5);
+				memcpy(moves+i*5,b,sizeof(int) * 5);
+				int x = values[j];
+				values[i] = values[j];
+				values[j] = x;
+			}
+		}
+	}
+	
 }
 
 int alphaBetaPrune(Board * board, int turn, int * move, int depth, int alpha, int beta, int eval){
 	
-	//if (END_TIME < time(NULL))
-	//	return eval == turn ? -MATE : MATE;
+	if (END_TIME < time(NULL))
+		return eval == turn ? -MATE : MATE;
 	
 	ApplyMove(board,move);
 	int * lastMove = board->LastMove;
 	board->LastMove = move;
 	
+	int * key = createKey(board);
+	int hash = createHash(key);
+	Node * node = getNode(TABLE,hash,key);
+	
+	if (node != NULL && node->depth >= depth){
+		int value = node->turn == turn ?  node->value : -node->value;
+		
+		if (node->type == EXACT){
+			RevertMove(board,move);
+			free(key);
+			return value;
+		}
+		
+		if (node->type == LOWERBOUND && node->value > alpha)
+			alpha = value;
+		else if(node->type == UPPERBOUND && node->value < beta)
+			beta = value;
+			
+		if (alpha >= beta){
+			RevertMove(board,move);
+			free(key);
+			return value;
+		}
+	}
 	
 	if (depth == 0){
 		int value = evaluateBoard(board, turn);
 		RevertMove(board,move);
 		board->LastMove = lastMove;
+		
+		if (node == NULL){
+			if (value <= alpha)
+				storeNode(TABLE,hash,createNode(key,value,depth,LOWERBOUND,turn));
+			else if (value >= beta)
+				storeNode(TABLE,hash,createNode(key,value,depth,UPPERBOUND,turn));
+			else
+				storeNode(TABLE,hash,createNode(key,value,depth,EXACT,turn));
+		} else 
+			free(key);
+		
 		return value;
 	}
 	
 	int best = 0, size = 0;
 	int * moves = getAllMoves(board,turn,&size);
 	int * moves_p = moves;
+	
+	TOTAL_MOVES_FOUND += size;
 	
 	if (size == 0){
 		best = validateMove(board,turn) == 1 ? 0 : -MATE;
@@ -107,14 +166,32 @@ int alphaBetaPrune(Board * board, int turn, int * move, int depth, int alpha, in
 			int value = -alphaBetaPrune(board,!turn,moves,depth-1,-beta,-alpha,eval);
 			
 			if (value > best)
-				best = value;
-				
+				best = value;	
 			if (best > alpha)
-				alpha = best;
-				
+				alpha = best;	
 			if (best >= beta)
 				break;
 		}
+	}
+	
+	if (node == NULL){
+		if (best <= alpha)
+			storeNode(TABLE,hash,createNode(key,best,depth,LOWERBOUND,turn));
+		else if (best >= beta)
+			storeNode(TABLE,hash,createNode(key,best,depth,UPPERBOUND,turn));
+		else
+			storeNode(TABLE,hash,createNode(key,best,depth,EXACT,turn));
+	} else if (depth > node->depth){
+		free(key);
+		node->turn = turn;
+		node->value = best;
+		
+		if (best <= alpha)
+			node->type = LOWERBOUND;
+		else if (best >= beta)
+			node->type = UPPERBOUND;
+		else
+			node->type = EXACT;
 	}
 	
 	board->LastMove = lastMove;
@@ -125,12 +202,12 @@ int alphaBetaPrune(Board * board, int turn, int * move, int depth, int alpha, in
 
 int evaluateBoard(Board * board, int turn){
 	TOTAL_BOARDS_SEARCHED += 1;
-	return evaluateMaterial(board, turn);
+	return 	evaluateMaterial(board, turn) + 
+			evalutePosition(board, turn) ;
 }
 
 int evaluateMaterial(Board * board, int turn){
 	int i, value = 0;
-	
 	for(i = 0; i < 64; i++){
 		if (board->TYPES[i] != EMPTY){
 			if (board->COLORS[i] == turn)
@@ -139,11 +216,21 @@ int evaluateMaterial(Board * board, int turn){
 				value -= MATERIAL_VALUES[board->TYPES[i]];
 		}
 	}
-	
 	return value;
 }
 
+int evalutePosition(Board * board, int turn){
+	int x,y, value = 0;
+	for(x = 2; x < 6; x++)
+		for(y = 2; y < 6; y++)
+			if (board->Types[x][y] == KNIGHT)
+				value += board->Colors[x][y] == turn ? VALUE_CENTRAL_KNIGHT : -VALUE_CENTRAL_KNIGHT;
+	
+	for(x = 3; x < 5; x++)
+		for(y = 3; y < 5; y++)
+			if (board->Types[x][y] != EMPTY)
+				value += board->Colors[x][y] == turn ? VALUE_CENTER_ATTACKED : -VALUE_CENTER_ATTACKED;
 
-
-
+	return value;
+}
 
