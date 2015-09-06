b25bbd1849888c374df727df200349bff91c47e4
AndyGrant <Andrew@grantnet.us>
2015-09-06 00:05:37 -0400

    .

--- New_Engine/Search.h
+++ New_Engine/Search.h
@@ -15,12 +15,22 @@
 
 #define VALUE_KING_SURROUNDINGS_ATTACKED 10
 
+typedef struct SearchThreadData{
+	Board * board;
+	int turn;
+	int * move;
+	int depth;
+	int alpha;
+	int beta;
+	int eval;
+} SearchThreadData;
+
 int getBestMoveIndex(Board * board, int turn);
 int endSearch(int index, int size, int * values, int * sorted, int * unsorted);
 int alphaBetaPrune(Board * board, int turn, int * moves , int depth, int alpha, int beta, int eval);
 
 void valueSort(int * values, int * moves, int size);
-int * hueristicSort(Board * board, int * moves, int size);
+void hueristicSort(Board * board, int * moves, int size, int turn);
 
 int evaluateBoard(Board * board, int turn);
 int evaluateMaterial(Board * board, int turn);
