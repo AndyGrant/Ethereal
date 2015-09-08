8ce6447a85899796d714dc843b12b6d7963a3dba
AndyGrant <Andrew@grantnet.us>
2015-09-07 21:37:46 -0400

    .

--- New_Engine/_Compile.sh
+++ New_Engine/_Compile.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-gcc -o3 TTable.c Search.c Engine.c -o foo.exe -L. -lpthreadGC2
+if [[ "$unamestr" == 'Linux' ]]; then
+	gcc -o3 TTable.c Search.c Engine.c -o foo.exe -L. -lpthread
+else
+	gcc -o3 TTable.c Search.c Engine.c -o foo.exe -L. -lpthreadGC2
+fi
