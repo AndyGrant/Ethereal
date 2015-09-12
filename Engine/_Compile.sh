e13f4442234032c2e03483aaa1dc104aeb428d3c
AndyGrant <Andrew@grantnet.us>
2015-09-12 18:22:31 -0400

    Platform specific compiling

--- Engine/_Compile.sh
+++ Engine/_Compile.sh
@@ -6,12 +6,12 @@ gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthrea
 OS="`uname`"
 case $OS in
   'Linux')
-   #gcc -o3 Engine.c TTable.c Search.c GetBestMove.c -o GetBestMove.exe -lpthread;;
-   ;;
+    gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
+    gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthread
+  ;;
+  
   *) 
-	#gcc -o3 Engine.c TTable.c Search.c GetBestMove.c -o GetBestMove.exe -lpthreadGC2;;
-	;;
+    gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
+    gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthreadgc2
+  ;;
 esac
-
-
-#3111214151999931999999999999999999999921991199990101010101010101000000000000000099999920991099999999999999999999301020405099993000111199
