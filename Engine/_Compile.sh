93460bd77972cfcc83c96df0511bd1c3149ff2e2
AndyGrant <Andrew@grantnet.us>
2015-09-12 18:19:27 -0400

    Created needed .exes for running the game

--- Engine/_Compile.sh
+++ Engine/_Compile.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-if [[ "$unamestr" == 'Linux' ]]; then
-	gcc -o3 TTable.c Search.c Engine.c -o foo.exe -L. -lpthread
-else
-	gcc -o3 TTable.c Search.c Engine.c -o foo.exe -L. -lpthreadGC2
-fi
+gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
+gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthreadgc2
+
+OS="`uname`"
+case $OS in
+  'Linux')
+   #gcc -o3 Engine.c TTable.c Search.c GetBestMove.c -o GetBestMove.exe -lpthread;;
+   ;;
+  *) 
+	#gcc -o3 Engine.c TTable.c Search.c GetBestMove.c -o GetBestMove.exe -lpthreadGC2;;
+	;;
+esac
+
+
+#3111214151999931999999999999999999999921991199990101010101010101000000000000000099999920991099999999999999999999301020405099993000111199
