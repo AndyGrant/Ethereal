b48c5fca60bf101da60b0c1e93b7e26ff6e601b1
AndrewGrant <andrew@grantnet.us>
2015-09-14 08:58:33 -0400

    More casing fixes

--- Engine/_Compile.sh
+++ Engine/_Compile.sh
@@ -3,12 +3,12 @@
 OS="`uname`"
 case $OS in
   'Linux')
-    gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
+    gcc -o3 Engine.c GetMoves.c -o getMoves.exe
     gcc -o3 Search.c TTable.c Engine.c GetEngineMove.c -o getEngineMove.exe -lpthread
   ;;
   
   *) 
-    gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
+    gcc -o3 Engine.c GetMoves.c -o getMoves.exe
     gcc -o3 Search.c TTable.c Engine.c GetEngineMove.c -o getEngineMove.exe -lpthreadgc2
   ;;
 esac
