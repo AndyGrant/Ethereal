5ec1fbd107ebde05b262d205d2262fec8b487433
AndrewGrant <andrew@grantnet.us>
2015-09-14 08:56:34 -0400

    Fixed unix casing

--- Engine/_Compile.sh
+++ Engine/_Compile.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
-gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthreadgc2
-
 OS="`uname`"
 case $OS in
   'Linux')
     gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
-    gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthread
+    gcc -o3 Search.c TTable.c Engine.c GetEngineMove.c -o getEngineMove.exe -lpthread
   ;;
   
   *) 
     gcc -o3 Engine.c GetMoves.c -o GetMoves.exe
-    gcc -o3 Search.c TTable.c Engine.c getEngineMove.c -o getEngineMove.exe -lpthreadgc2
+    gcc -o3 Search.c TTable.c Engine.c GetEngineMove.c -o getEngineMove.exe -lpthreadgc2
   ;;
 esac
