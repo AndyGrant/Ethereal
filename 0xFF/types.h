c2e37255a2200829274459ae6b419a3accffe00c
AndyGrant <Andrew@grantnet.us>
2015-10-11 04:57:21 -0400

    Testing move macros, added additional move macros and changed syntax from c++ to pre-processor C

--- 0xFF/types.h
+++ 0xFF/types.h
@@ -2,74 +2,81 @@
 #define TYPES_H
 
 /* Color Definitions */
-const int ColourWhite 	= 0;
-const int ColourBlack 	= 1;
-const int ColourNone 		= 2;
+#define ColourWhite 		(0)
+#define ColourBlack 		(1)
+#define ColourNone 			(2)
 
-const int WhiteFlag 		= 0 << 0;
-const int BlackFLag 		= 1 << 0;
-const int NoneFlag			= 1 << 1;
+#define WhiteFlag 			(0 << 0)
+#define BlackFlag 			(1 << 0)
+#define NoneFlag				(1 << 1)
 
 /* Piece Definitions */
-const int PawnFlag 			= 1 << 2;
-const int KnightFlag 		= 1 << 3;
-const int BishopFlag 		= 1 << 4;
-const int RookFlag 			= 1 << 5;
-const int QueenFlag 		= 1 << 6;
-const int KingFlag 			= 1 << 7;
+#define		PawnFlag 			(1 << 2)
+#define		KnightFlag 		(1 << 3)
+#define		BishopFlag 		(1 << 4)
+#define		RookFlag 			(1 << 5)
+#define		QueenFlag 		(1 << 6)
+#define		KingFlag 			(1 << 7)
 
-const int NonePiece   	= NoneFlag  	| ColourNone;
-const int WhitePawn			= PawnFlag		| WhiteFlag;
-const int BlackPawn			= PawnFlag		| BlackFlag;
-const int WhiteKnight 	= KnightFlag	| WhiteFlag;
-const int BlackKnight		= KnightFlag	| BlackFlag;
-const int WhiteBishop 	= BishopFlag	| WhiteFlag;
-const int BlackBishop 	= BishopFlag	| BlackFlag;
-const int WhiteRook			= RookFlag		| WhiteFlag;
-const int BlackRook 		= RookFlag		| BlackFlag;
-const int WhiteQueen 		= QueenFlag		| WhiteFlag;
-const int BlackQueen 		= QueenFlag		| BlackFlag;
-const int WhiteKing 		= KingFlag		| WhiteFlag;
-const int BlackKing 		= KingFlag		| BlackFlag;
+#define 	NonePiece   	(NoneFlag  	| ColourNone)
+#define 	WhitePawn			(PawnFlag		| WhiteFlag)
+#define 	BlackPawn			(PawnFlag		| BlackFlag)
+#define 	WhiteKnight 	(KnightFlag	| WhiteFlag)
+#define 	BlackKnight		(KnightFlag	| BlackFlag)
+#define 	WhiteBishop 	(BishopFlag	| WhiteFlag)
+#define 	BlackBishop 	(BishopFlag	| BlackFlag)
+#define 	WhiteRook			(RookFlag		| WhiteFlag)
+#define 	BlackRook 		(RookFlag		| BlackFlag)
+#define 	WhiteQueen 		(QueenFlag	| WhiteFlag)
+#define 	BlackQueen 		(QueenFlag	| BlackFlag)
+#define 	WhiteKing 		(KingFlag		| WhiteFlag)
+#define 	BlackKing 		(KingFlag		| BlackFlag)
 
 /* Move Type Definitions */
-const int NormalFlag 					= 1 << 24;
-const int CastleFlag 					= 1 << 25;
-const int EnpassFlag 					= 1 << 26;
-const int PromotionFlag 			= 1 << 27;
+#define 	NormalFlag 					(1 << 24)
+#define 	CastleFlag 					(1 << 25)
+#define 	EnpassFlag 					(1 << 26)
+#define 	PromotionFlag 			(1 << 27)
+#define 	PromoteKnightFlag 	(1 << 28)
+#define 	PromoteishopFlag 		(1 << 29)
+#define 	PromoteRookFlag 		(1 << 30)
+#define 	PromoteQueenFlag 		(1 << 31) 
+#define 	PromoteFlags				(15<< 28)
 
-const int PromoteKnightFlag 	= 1 << 28;
-const int PromoteishopFlag 		= 1 << 29;
-const int PromoteRookFlag 		= 1 << 30;
-const int PromoteQueenFlag 		= 1 << 31; 
-const int PromoteFlags				= 15 << 28;
-const int PromoteTypes[9]			= {0, KnightFlag, BishopFlag, 0, 
+static	 	PromoteTypes[9]			= {0, KnightFlag, BishopFlag, 0, 
 																RookFlag, 0, 0, 0, QueenFlag};
+																
+/* Search Definitions */
+#define		MaxDepth	(128)
+#define		MaxMoves	(256)
 
-/* Macro Definitions */																
-#define PIECE_COLOUR					(piece) (peice & 3);
-#define PIECE_IS_WHITE				(piece) (!PIECE_COLOUR(piece))
-#define PIECE_IS_BLACK				(piece) (PIECE_COLOUR(piece))
+/* Colour Macro Definitions */																
+#define PIECE_COLOUR(piece) 				(peice & 3)
+#define PIECE_IS_WHITE(piece) 			(!PIECE_COLOUR(piece))
+#define PIECE_IS_BLACK(piece) 			(PIECE_COLOUR(piece))
 
-#define PIECE_IS_NONE					(piece) (piece & NonePiece)
-#define PIECE_IS_PAWN					(piece) (piece & PawnFlag)
-#define PIECE_IS_KNIGHT				(piece) (piece & KnightFlag)
-#define PIECE_IS_BISHOP				(piece) (piece & BishopFlag)
-#define PIECE_IS_ROOK					(piece) (piece & RookFlag)
-#define PIECE_IS_QUEEN				(piece) (piece & QueenFlag)
-#define PIECE_IS_KING					(piece) (piece & KingFlag)
+/* Piece Macro Definitions */
+#define PIECE_IS_NONE(piece) 				(piece & NonePiece)
+#define PIECE_IS_PAWN(piece) 				(piece & PawnFlag)
+#define PIECE_IS_KNIGHT(piece) 			(piece & KnightFlag)
+#define PIECE_IS_BISHOP(piece) 			(piece & BishopFlag)
+#define PIECE_IS_ROOK(piece) 				(piece & RookFlag)
+#define PIECE_IS_QUEEN(piece) 			(piece & QueenFlag)
+#define PIECE_IS_KING(piece) 				(piece & KingFlag)
 
-#define MOVE_IS_NORMAL				(move) (move & NormalFlag)
-#define MOVE_IS_CASTLE				(move) (move & CastleFlag)
-#define MOVE_IS_ENPASS				(move) (move & EnpassFlag)
-#define MOVE_IS_PROMOTION			(move) (move & PromotionFlag)
+/* Move Type Macro Definitions */
+#define MOVE_IS_NORMAL(move) 				(move & NormalFlag)
+#define MOVE_IS_CASTLE(move) 				(move & CastleFlag)
+#define MOVE_IS_ENPASS(move) 				(move & EnpassFlag)
+#define MOVE_IS_PROMOTION(move) 		(move & PromotionFlag)
 
-#define MOVE_GET_FROM					(move) (move & (0b11111111 << 0))
-#define MOVE_GET_TO						(move) (move & (0b11111111 << 8))
-#define MOVE_GET_CAPTURE_TYPE (move) (move & (0b11111111 << 16))
-#define MOVE_GET_CASTLE_FLAGS (move) (move & (0b11111111 << 28))
-#define MOVE_GET_PROMOTE_TYPE	(move, colour) (PromoteTypes[((move&PromoteFlags)>>28)]+colour)
-	
+/* Move Decode Macro Definitions */
+#define MOVE_GET_FROM(move)		 			((move & (0b11111111 <<  0)) >>  0)
+#define MOVE_GET_TO(move) 					((move & (0b11111111 <<  8)) >>  8)
+#define MOVE_GET_CAPTURE_TYPE(move) ((move & (0b11111111 << 16)) >> 16)
+#define MOVE_GET_CASTLE_FLAGS(move) ((move & (0b00001111 << 28)) >> 25)
+#define MOVE_GET_PROMOTE_TYPE(m,c) 	(PromoteTypes[((m&PromoteFlags)>>28)]+c)
+
 /* Structure Definitions */
 
 typedef uint32_t move_t;
@@ -78,6 +85,11 @@ typedef uint32_t move_t;
 //  bits  16-23: Capture Type
 //	bits  24-27: Move Type
 //  bits  28-31: Castle Changes
+
+typedef struct transposition_t {
+	
+	
+} transposition_t;
 
 typedef struct board_t {
 	int positions[0xFF];
@@ -104,13 +116,13 @@ typedef struct principle_variation_t {
 
 typedef struct search_tree_t {
 	int ply;
+	int nodes_searched;
+	
 	board_t board;
+	
 	move_t killer_moves[MaxDepth][3];
 	principle_variation_t principle_variation;
 	
 } search_tree_t;
-
-
-	
 
 #endif
