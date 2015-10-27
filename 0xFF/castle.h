9e3c6a0660869327ee1ef6e913779d092f2cd832
AndrewGrant <andrew@grantnet.us>
2015-10-27 13:06:44 -0400

    Cleaned up Macro operation order

--- 0xFF/castle.h
+++ 0xFF/castle.h
@@ -16,16 +16,16 @@
 #define BlackKingCastle		(1 << 2)
 #define BlackQueenCastle	(1 << 3)
 
-#define KING_HAS_RIGHTS(t,r)	(r & (1 << (t*2)))
-#define QUEEN_HAS_RIGHTS(t,r)	(r & (2 << (t*2)))
+#define KING_HAS_RIGHTS(t,r)	((r) & (1 << ((t)*2)))
+#define QUEEN_HAS_RIGHTS(t,r)	((r) & (2 << ((t)*2)))
 
-#define CREATE_KING_RIGHTS(t)	(1 << (t*2))
-#define CREATE_QUEEN_RIGHTS(t)	(2 << (t*2))
+#define CREATE_KING_RIGHTS(t)	(1 << ((t)*2))
+#define CREATE_QUEEN_RIGHTS(t)	(2 << ((t)*2))
 
-#define IS_LEFT_ROOK(t,s)		(s==(180-(t*(180-68))))
-#define IS_RIGHT_ROOK(t,s)		(s==(187-(t*(187-75))))
+#define IS_LEFT_ROOK(t,s)		((s)==(180-((t)*(180-68))))
+#define IS_RIGHT_ROOK(t,s)		((s)==(187-((t)*(187-75))))
 
 /* Castle Macro Definitions */
-#define CREATE_CASTLE_RIGHTS(a,b,c,d) ((a<<0)+(b<<1)+(c<<2)+(d<<3))
+#define CREATE_CASTLE_RIGHTS(a,b,c,d) (((a)<<0)+((b)<<1)+((c)<<2)+((d)<<3))
 
 #endif
