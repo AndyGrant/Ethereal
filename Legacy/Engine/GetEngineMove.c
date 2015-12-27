34eba1603e97f4635298e1b282718381ea3634b0
AndyGrant <Andrew@grantnet.us>
2015-12-27 05:32:14 -0500

    Relocated previous version which is no longer in development

---
+++ Legacy/Engine/GetEngineMove.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Board.h"
+#include "MoveGen.h"
+#include "Move.h"
+#include "Search.h"
+
+char * encodeBoard(Board * board){
+	char * str = malloc(sizeof(char) * 137);
+	
+	int i;
+	for(i = 0; i < 64; i++){
+		str[i*2] = board->TYPES[i] + '0';
+		str[i*2+1] = board->COLORS[i] + '0';
+	}
+	
+	str[128] = board->Castled[0] + '0';
+	str[129] = board->Castled[1] + '0';
+	str[130] = board->ValidCastles[0][0] + '0';
+	str[131] = board->ValidCastles[0][1] + '0';
+	str[132] = board->ValidCastles[1][0] + '0';
+	str[133] = board->ValidCastles[1][1] + '0';
+	
+	if (board->LastMove[0] != 4){
+		str[134] = '9';
+		str[135] = '9';
+	} else {
+		str[134] = (board->LastMove[2] / 10) + '0';
+		str[135] = (board->LastMove[2] % 10) + '0';
+	}
+	str[136] = 0;
+	return str;
+}
+
+int main(int argc, char * argv[]){
+	Board * board = createBoard(argv[1]);
+	int turn = argv[2][0] == '0' ? WHITE : BLACK;
+	int bestIndex = getBestMoveIndex(board,turn);
+  
+  if( bestIndex == -1){
+    printf("CHECKMATE");
+  }
+  else if(bestIndex == -2)
+    printf("STALEMATE");
+  else{
+    int size = 0;
+    int * moves = getAllMoves(board,turn,&size);
+    ApplyMove(board,(moves + (5*bestIndex)));
+    printf("NEWBOARD=%s",encodeBoard(board));
+  }
+}
+	
---
+++ Legacy/SelfPlay/SelfPlay.py
@@ -0,0 +1,131 @@
+from Tkinter import *
+from subprocess import PIPE, Popen
+import time, sys
+
+ENGINE_A = sys.argv[1]+"/getEngineMove.exe";
+ENGINE_B = sys.argv[2]+"/getEngineMove.exe";
+
+ENGINE_DIRS = [ENGINE_A, ENGINE_B]
+
+WINNER = [sys.argv[2],sys.argv[1]]
+
+EXIT_ON_COMPLETION = bool(sys.argv[3])
+
+WHITE = 0
+BLACK = 1
+
+STARTING_BOARD = "3111214151211131010101010101010199999999999999999999999999999999999999999999999999999999999999990000000000000000301020405020103000111199 1"
+
+# StaleMate Test
+# STARTING_BOARD =  "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999950999999999999999999995100000099 0"
+
+root = Tk()
+
+IMAGES = {}
+IMAGES['00'] = PhotoImage(file="../GUI/_Assets/WhitePawn.gif")
+IMAGES['10'] = PhotoImage(file="../GUI/_Assets/WhiteKnight.gif")
+IMAGES['20'] = PhotoImage(file="../GUI/_Assets/WhiteBishop.gif")
+IMAGES['30'] = PhotoImage(file="../GUI/_Assets/WhiteRook.gif")
+IMAGES['40'] = PhotoImage(file="../GUI/_Assets/WhiteQueen.gif")
+IMAGES['50'] = PhotoImage(file="../GUI/_Assets/WhiteKing.gif")
+IMAGES['01'] = PhotoImage(file="../GUI/_Assets/BlackPawn.gif")
+IMAGES['11'] = PhotoImage(file="../GUI/_Assets/BlackKnight.gif")
+IMAGES['21'] = PhotoImage(file="../GUI/_Assets/BlackBishop.gif")
+IMAGES['31'] = PhotoImage(file="../GUI/_Assets/BlackRook.gif")
+IMAGES['41'] = PhotoImage(file="../GUI/_Assets/BlackQueen.gif")
+IMAGES['51'] = PhotoImage(file="../GUI/_Assets/BlackKing.gif")
+IMAGES['OPTION'] = PhotoImage(file="../GUI/_Assets/Option.gif")
+IMAGES["BOARD"] = PhotoImage(file="../GUI/_Assets/Board.gif")
+
+class ChessGUI(Frame):
+  def __init__(self, parent):
+    self.parent = parent
+    Frame.__init__(self,parent)
+    self.initInterface(parent)
+    self.game = ChessGame(STARTING_BOARD,int (not int(STARTING_BOARD[-1])),self.canvas)
+    
+    
+  def initInterface(self, parent):
+    self.parent.title("Ethereal Chess : Andrew Grant")
+    self.pack(fill=BOTH,expand=1)
+    self.canvas = Canvas(self,width=420,height=420)
+    self.canvas.pack()
+    
+    
+class ChessGame():
+  def __init__(self, board, turn, canvas):
+    self.canvas = canvas;
+    self.selected = None
+    self.board = board
+    self.turn = turn
+    self.drawn = []
+    self.pastBoards = []
+    
+    
+    self.drawBoard()
+    Tk.update(root)
+    
+    while True:
+      status = self.makeEngineMove()
+      
+      
+      if status == -1:
+        self.logGame(WINNER[turn])
+        break
+      elif self.stalemate():
+        self.logGame(.5)
+        break
+      elif status == -2:
+        self.logGame(.5)
+        break
+      
+      self.drawBoard()
+      Tk.update(root)
+      self.turn = (1+self.turn)%2   
+
+  def stalemate(self):
+    for board in self.pastBoards:
+      if self.pastBoards.count(board) >= 4:
+        return True
+    return False
+      
+  
+  def logGame(self, result):
+    with open('output.txt','a') as file:
+      file.write(" ".join([sys.argv[1],"VS",sys.argv[2],"Result=",str(result),"\n"]))
+    if EXIT_ON_COMPLETION:
+      sys.exit(1)
+      
+  def drawBoard(self):
+    for f in self.drawn:
+      self.canvas.delete(f)
+      
+    self.drawn.append(self.canvas.create_image((0,0),anchor=NW,image=IMAGES["BOARD"]))
+    
+    if self.selected != None:
+      for f in self.moves.getEnds(self.selected):
+        self.drawn.append(self.canvas.create_image((10+50*(f%8),10+50*(f/8)),anchor=NW,image=IMAGES['OPTION']))
+    
+    for f in range(0,128,2):
+      p = self.board[f] + self.board[f+1]
+      if p != "99":
+        x,y = 10 + 50 * (f/2/8), 10 + 50 * (f/2%8)
+        self.drawn.append(self.canvas.create_image((y,x),anchor=NW,image=IMAGES[p]))    
+    
+  def makeEngineMove(self):
+    proc = Popen([ENGINE_DIRS[self.turn],self.board,str((self.turn))], stdout=PIPE,shell=False)
+    (out, err) = proc.communicate()
+    #print out[0:out.find("NEWBOARD=")]
+    if "CHECKMATE" in out:
+      return -1
+    if "STALEMATE" in out:
+      return -2
+    self.board = out[out.find("NEWBOARD=") + len("NEWBOARD="):]
+    print self.board + " " + str(self.turn)
+    self.pastBoards.append(self.board+" "+str(self.turn))
+    return 1
+    
+root.geometry("420x420")
+root.resizable(0,0)
+app = ChessGUI(root)
+root.mainloop()
