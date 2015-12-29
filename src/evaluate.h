484de2e563d1b40c78a80c407702e84ae6a6f989
AndyGrant <Andrew@grantnet.us>
2015-12-29 04:34:46 -0500

    Additional evaluation improvements

--- src/evaluate.h
+++ src/evaluate.h
@@ -47,33 +47,11 @@ static int BishopValueMap[64] = {
 	 -5, -1, -1, -1, -1, -1, -1, -5	
 };
 
-static int RookValueMap[64] = {
-	 -1, -1,  1,  3,  3, -1, -1, -1,
-	 -1,  0,  0,  2,  2,  0,  0, -1, 
-	  0,  2,  3,  1,  1,  3,  2,  0, 
-	  1,  1,  2, -2, -2,  2,  1,  1, 
-	  1,  1,  2, -2, -2,  2,  1,  1, 
-	  0,  2,  3,  1,  1,  3,  2,  0, 
-	 -1,  0,  0,  2,  2,  0,  0, -1, 
-	 -1, -1,  1,  3,  3, -1, -1, -1
-};
-
-static int QueenValueMap[64] = {
-	 -2, -1, -1, -1, -1, -1, -1, -2,
-	  1,  2,  3,  2,  2,  3,  2,  1,
-	  4,  4,  5,  3,  3,  5,  4,  4,
-	  0,  6,  5,  7,  7,  5,  6,  0,
-	  0,  6,  5,  7,  7,  5,  6,  0,
-	  4,  4,  5,  3,  3,  5,  4,  4,
-	  1,  2,  3,  2,  2,  3,  2,  1,
-	 -2, -1, -1, -1, -1, -1, -1, -2,
-};
-
 #define WHITE_PAWN_POSITION_VALUE(p)	(WhitePawnValueMap[CONVERT_256_TO_64((p))])
 #define BLACK_PAWN_POSITION_VALUE(p)	(BlackPawnValueMap[CONVERT_256_TO_64((p))])
-#define QUEEN_POSITION_VALUE(p)			(QueenValueMap[CONVERT_256_TO_64((p))])
-#define ROOK_POSITION_VALUE(p)			(RookValueMap[CONVERT_256_TO_64((p))])
-#define BISHOP_POSITION_VALUE(p)		(BishopValueMap[CONVERT_256_TO_64((p))])
 #define KNIGHT_POSITION_VALUE(p)		(KnightValueMap[CONVERT_256_TO_64((p))])
+#define BISHOP_POSITION_VALUE(p)		(BishopValueMap[CONVERT_256_TO_64((p))])
+#define ROOK_POSITION_VALUE(p)			(0)
+#define QUEEN_POSITION_VALUE(p)			(0)
 
 #endif
