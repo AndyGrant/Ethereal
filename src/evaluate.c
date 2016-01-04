d2dc91e8775291822c2276fc4d39543d89370558
AndyGrant <Andrew@grantnet.us>
2016-01-04 01:06:52 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -40,145 +40,78 @@ int evaluate_board(){
 	int black = evaluate_player(ColourBlack,pawn_info);
 	
 	if (board.turn == ColourWhite)
-		return 10 + white - black;
-	return 10 + black - white;
+		return 0 + white - black;
+	return 0 + black - white;
 }
 
-int evaluate_player(int turn, int pawn_info[2][10]){
-	int value = 0, *location, i;
+int evaluate_player(int turn, int pawn_info[2][10]){	
 	
-	int num_knights = 0;
-	int num_bishops = 0;
-	int rook_location = -1;
+	int value 		= 0;
+	int *location	= NULL;
+	int pstart 		= turn == ColourWhite ? 0 : 7;
+	int pend   		= turn == ColourWhite ? 7 : 0;
+	int is_endgame  = (board.piece_counts[0] + board.piece_counts[1] <= 7);
+	int rook_sq     = -1;
+	int rook_stack  = 0;
 	
 	for(location = &(board.piece_locations[turn][0]); *location != -1; location++){
-		int type = board.squares[*location];
+		int type = board.squares[*location] & ~1;
 		int sq256 = *location;
-		int sq64 = CONVERT_256_TO_64(sq256);
+		int sq64= CONVERT_256_TO_64(sq256);
 		int col = 1 + sq64 % 8;
+		
+		int truesq = turn == ColourWhite ? sq64 : inv[sq64];
+		
 		
 		switch(type){
 			case WhiteKnight:
-				value += KnightValue;
-				value += KnightValueMap[sq64];
-				num_knights++;
-				break;
-			
-			case BlackKnight:
-				value += KnightValue;
-				value += KnightValueMap[inv[sq64]];
-				num_knights++;
-				break;
+				value += KnightValue + KnightValueMap[truesq];
+			break;
 			
 			case WhiteBishop:
-				value += BishopValue;
-				value += BishopValueMap[sq64];
-				num_bishops++;
-				break;
+				value += BishopValue + BishopValueMap[truesq];
+			break;
 			
-			case BlackBishop:
-				value += BishopValue;
-				value += BishopValueMap[inv[sq64]];
-				num_bishops++;
-				break;
-				
 			case WhiteRook:
 				value += RookValue;
-				if (pawn_info[ColourWhite][col] == 0){
-					if (pawn_info[ColourBlack][col] == 7){
-						value += OpenFileRookValue;
-						if (rook_location == -1) 
-							rook_location = sq64;
-						else if (rook_location % 8 == sq64 % 8)
-							value += RooksOnSameOpenFileValue;
-					}
-					else{
-						value += SemiOpenFileRookValue;
-						if (rook_location == -1) 
-							rook_location = sq64;
-						else if (rook_location % 8 == sq64 % 8)
-							value += RooksOnSameOpenFileValue;
-					}
-				}
-				
-				if (sq64 / 16 == 1)
-					value += SeventhRankRookValue;
-				if (sq64 / 16 == 0)
-					value += EightRankRookValue;
-				
-				break;
 			
-			case BlackRook:
-				value += RookValue;
-				if (pawn_info[ColourBlack][col] == 7){
-					if (pawn_info[ColourWhite][col] == 0){
+				if (rook_sq == -1)
+					rook_sq = truesq;
+				else if (truesq % 8 == rook_sq % 8)
+					rook_stack = 1;
+				
+				if (pawn_info[board.turn][col] == pstart){
+					if (pawn_info[!board.turn][col] == pend){
 						value += OpenFileRookValue;
-						if (rook_location == -1) 
-							rook_location = sq64;
-						else if (rook_location % 8 == sq64 % 8)
+						if (rook_stack)
 							value += RooksOnSameOpenFileValue;
-					}
-					else{
+					} else{
 						value += SemiOpenFileRookValue;
-						if (rook_location == -1) 
-							rook_location = sq64;
-						else if (rook_location % 8 == sq64 % 8)
+						if (rook_stack)
 							value += RooksOnSameSemiOpenFileValue;
 					}
 				}
 				
-				if (sq64 / 16 == 6)
+				if (truesq / 8 == 1)
 					value += SeventhRankRookValue;
-				if (sq64 / 16 == 7)
-					value += EightRankRookValue;
+				else if (truesq / 8 == 1)
+					value += EighthRankRookValue;
 				
-				break;
+			break;
 				
 			case WhiteQueen:
 				value += QueenValue;
-				break;
-			
-			case BlackQueen:
-				value += QueenValue;
-				break;
+			break;
 			
 			case WhiteKing:
-				if (board.piece_counts[0] + board.piece_counts[1] <= 7)
-					value += KingEndValueMap[sq64] / 2;
-				else{
-					value += KingEarlyValueMap[sq64];
-					if (board.squares[sq256-16] == WhitePawn)
-						value += PawnInfrontOfKingValue;
-					if (board.squares[sq256-15] == WhitePawn)
-						value += PawnInfrontOfKingValue;
-					if (board.squares[sq256-17] == WhitePawn)
-						value += PawnInfrontOfKingValue;
-				}
-				
-				break;
-				
-			case BlackKing:
-				if (board.piece_counts[0] + board.piece_counts[1] <= 7)
-					value += KingEndValueMap[inv[sq64]] / 2;
-				else{
-					value += KingEarlyValueMap[inv[sq64]];
-					if (board.squares[sq256+16] == BlackPawn)
-						value += PawnInfrontOfKingValue;
-					if (board.squares[sq256+17] == BlackPawn)
-						value += PawnInfrontOfKingValue;
-					if (board.squares[sq256+15] == BlackPawn)
-						value += PawnInfrontOfKingValue;
-				}
-				break;
-			
+				if (is_endgame)
+					value += KingEndValueMap[truesq];
+				else
+					value += KingEarlyValueMap[truesq];
+			break;
 		}
 	}
-	
-	if (num_bishops == 2)	
-		value += HasBothBishopsValue;
-	
-	int is_end_game = (board.piece_counts[0] + board.piece_counts[1] <= 7);
-	
+		
 	for(location = &(board.pawn_locations[turn][0]); *location != -1; location++){
 		int sq256 = *location;
 		int sq64 = CONVERT_256_TO_64(sq256);
@@ -188,13 +121,13 @@ int evaluate_player(int turn, int pawn_info[2][10]){
 		value += PawnValue;
 		
 		if (turn == ColourWhite){
-			if (is_end_game)
+			if (is_endgame)
 				value += PawnEndValueMap[sq64];
 			else
 				value += PawnEarlyValueMap[sq64];
 		}
 		else{
-			if (is_end_game)
+			if (is_endgame)
 				value += PawnEndValueMap[inv[sq64]];
 			else
 				value += PawnEarlyValueMap[inv[sq64]];
--- src/search.c
+++ src/search.c
@@ -23,14 +23,9 @@ extern ttable_t table;
 time_t StartTime, EndTime;
 int EvaluatingPlayer;
 
-int SuccessfulLateMoveReductions = 0;
-int FailedLateMoveReductions = 0;
-int SuccessfulNullWindow = 0;
-int FailedNullWindow = 0;
-
 move_t get_best_move(int alloted_time){
 	int depth, i, size = 0; 	
-	alloted_time = 10;
+	alloted_time = 20;
 	
 	StartTime = time(NULL);
 	EndTime = StartTime + alloted_time;
@@ -44,43 +39,17 @@ move_t get_best_move(int alloted_time){
 	
 	int old_hash = board.hash;
 	
-	for(depth = 1; depth < MaxDepth; depth++){
+	for(depth = 0; depth < MaxDepth; depth++){
 		
-		int slmr = SuccessfulLateMoveReductions;
-		int flmr = FailedLateMoveReductions;
-		int snw = SuccessfulNullWindow;
-		int fnw = FailedNullWindow;
-		int rnodes = tree.raw_nodes;
-		int anodes = tree.alpha_beta_nodes;
-		int qnodes = tree.quiescence_nodes;
-		
-		int value = alpha_beta_prune(&(tree.principle_variation),depth,-CheckMate,CheckMate);
-		
-		printf("Search Depth        : %d\n",depth);
-		printf("Successful LMR      : %d\n",SuccessfulLateMoveReductions-slmr);
-		printf("Failed LMR          : %d\n",FailedLateMoveReductions-flmr);
-		printf("Successful NW       : %d\n",SuccessfulNullWindow-snw);
-		printf("Failed NW           : %d\n",FailedNullWindow-fnw);
-		printf("Raw Nodes           : %d\n",tree.raw_nodes - rnodes);
-		printf("Alpha Nodes         : %d\n",tree.alpha_beta_nodes - anodes);
-		printf("Quiescence Nodes    : %d\n",tree.quiescence_nodes - qnodes);
-		
-		printf("Principle Variation : ");
-		for(i = 0; i < tree.principle_variation.length; i++){
-			print_move_t(tree.principle_variation.line[i]);
-			printf(" -> ");
-		}
-		printf("\nValue               : %s%.2f\n",value >= 0 ? "+" : "", (float)value/PawnValue);
-		
+		int value = alpha_beta_prune(&(tree.principle_variation),depth,-CheckMate,CheckMate);		
 		printf("info depth %d score cp %d time %d nodes %d pv ",depth,(100*value)/PawnValue,1000 * (time(NULL) - StartTime), tree.raw_nodes);
 		for(i = 0; i < tree.principle_variation.length; i++){
 			print_move_t(tree.principle_variation.line[i]);
 			printf(" ");
 		}
 		printf("\n");
-		printf("------------------------------\n");
 		
- 		if (EndTime - ((float)(alloted_time) * .7) < time(NULL))
+ 		if (EndTime - ((float)(alloted_time) * .8) < time(NULL))
  			break;	
 	}
 	
@@ -96,7 +65,7 @@ void init_search_tree_t(){
 	tree.quiescence_nodes = 0;
 	tree.principle_variation.length = 0;
 	memset(&(tree.principle_variation.line[0]),0,sizeof(move_t) * MaxDepth);
-	memset(&(tree.killer_moves[0][0]),0,sizeof(move_t) * 5 * MaxDepth);
+	memset(&(tree.killer_moves[0][0]),0,sizeof(move_t) * 3 * MaxDepth);
 }
 
 int alpha_beta_prune(principle_variation_t * pv, int depth, int alpha, int beta){
@@ -114,13 +83,10 @@ int alpha_beta_prune(principle_variation_t * pv, int depth, int alpha, int beta)
 	if (depth == 0){
 		tree.raw_nodes--;
 		tree.alpha_beta_nodes--;
-		int qs = quiescence_search(alpha,beta);
-		return qs == -CheckMate ? evaluate_board(board) : qs;
+		return quiescence_search(alpha,beta);
 	}	
 	
 	tree.ply++;	
-	
-	int d = 1;
 	
 	int valid_size = 0, size = 0;
 	move_t moves[MaxMoves];
@@ -129,83 +95,83 @@ int alpha_beta_prune(principle_variation_t * pv, int depth, int alpha, int beta)
 	
 	move_t best_move = 0;
 	int i, value, best = -CheckMate - depth;
+	
 	for (i = 0; i < size; i++){
+		
 		apply_move(moves[i]);
+		
 		if (is_not_in_check(!(board.turn))){
 			valid_size++;
 			
+			// Determine three-fold repitition
 			int z, repititions = 0;
 			for(z = board.hash_entries-1; z >= 0; z-=2)
 				if (board.hash_history[z] == board.hash)
 					repititions++;
 			
-			if (repititions >= 3)
+			// Penalty for approaching repitition
+			if (repititions == 2)
+				value = -100;
+			
+			/* Determine Value Of Node */
+			if (repititions >= 3){
 				value = 0;
-			
-			else if (i == 0)
-				value = -alpha_beta_prune(&lpv,depth-1,-beta,-alpha);
-			
-			else if (valid_size > sqrt(size) + 2 && depth >= 3 && IS_EMPTY(MOVE_GET_CAPTURE(moves[i])) && is_not_in_check(board.turn)){
 				
-				if (depth >= 9 && valid_size > sqrt(size) * 6)
+			} else if (i == 0){
+				value = -alpha_beta_prune(&lpv,depth-1,-beta,-alpha);
+				
+			} else if (valid_size > sqrt(size) && depth >= 3 && IS_EMPTY(MOVE_GET_CAPTURE(moves[i])) && is_not_in_check(board.turn)){
+				if (depth >= 9 && valid_size > sqrt(size) * 4)
 					value = -alpha_beta_prune(&lpv,depth-4,-alpha-1,-alpha);
-				else if (depth >= 6 && valid_size > sqrt(size) * 3)
+				else if (depth >= 6 && valid_size > sqrt(size) * 2)
 					value = -alpha_beta_prune(&lpv,depth-3,-alpha-1,-alpha);
 				else
 					value = -alpha_beta_prune(&lpv,depth-2,-alpha-1,-alpha);
 					
-		
-				if (value > alpha){
-					
+				if (value > alpha){					
 					value = -alpha_beta_prune(&lpv,depth-1,-alpha-1,-alpha);
 					
-					if (value > alpha && value < beta){
+					if (value > alpha && value < beta)
 						value = -alpha_beta_prune(&lpv,depth-1,-beta,-alpha);
-						FailedNullWindow++;
-					} else
-						SuccessfulNullWindow++;
-						
-					FailedLateMoveReductions++;
 				}
-				else
-					SuccessfulLateMoveReductions++;
 				
-			}
-				
-			else {
+			} else {
 				value = -alpha_beta_prune(&lpv,depth-1,-alpha-1,-alpha);
 					
-				if (value > alpha && value < beta){
+				if (value > alpha && value < beta)
 					value = -alpha_beta_prune(&lpv,depth-1,-beta,-alpha);
-					FailedNullWindow++;
-				} else
-					SuccessfulNullWindow++;
 			}
+			/* End Determine Value Of Node */
 			
+			
+			// Adjust Alpha Beta Windows And Update PV
 			if (value > best){
 				best = value;
-				best_move = moves[i];
-			}
-			
-			if (best > alpha){
-				alpha = best;
 				
-				if (lpv.length != -1){
-					pv->line[0] = moves[i];
-					memcpy(pv->line + 1, lpv.line, sizeof(move_t) * lpv.length);
-					pv->length = lpv.length + 1;
+				if (best > alpha){
+					alpha = best;
+				
+					if (lpv.length != -1){
+						pv->line[0] = moves[i];
+						memcpy(pv->line + 1, lpv.line, sizeof(move_t) * lpv.length);
+						pv->length = lpv.length + 1;
+					}
 				}
 			}
 			
+			
+			// Terminate Search If Cut-Off Found
 			if (alpha > beta){
 				update_killer_heuristic(moves[i]);
 				revert_move(moves[i]); 
 				break;
 			}
 		}
+		
 		revert_move(moves[i]);
 	}
 	
+	// If Legal Moves Were Found And In Check Return DRAW
 	if (valid_size == 0 && is_not_in_check(board.turn))
 		best = 0;
 	
@@ -220,12 +186,6 @@ int quiescence_search(int alpha, int beta){
 	tree.raw_nodes++;
 	tree.quiescence_nodes++;
 	
-	if (EndTime < time(NULL)){
-		tree.ply--;
-		return board.turn == EvaluatingPlayer ? -CheckMate : CheckMate;
-	}
-	
-
 	ttentry_t entry = get_entry(board.hash);
 	if (entry != 0){
 		int rawv = ENTRY_GET_VALUE(entry);
@@ -254,7 +214,7 @@ int quiescence_search(int alpha, int beta){
 	
 	move_t moves[MaxMoves];
 	gen_all_captures(&(moves[0]),&size);
-	basic_heuristic(&(moves[0]),size);	
+	quiescence_heuristic(&(moves[0]),size);	
 	
 	for(i = 0; i < size; i++){
 		apply_move(moves[i]);	
@@ -301,8 +261,26 @@ void basic_heuristic(move_t * moves, int size){
 	int i, values[size];
 	for(i = 0; i < size; i++){
 		int cap = MOVE_GET_CAPTURE(moves[i]);
-		values[i] = 25*(!IS_EMPTY(cap) * cap) / board.squares[MOVE_GET_FROM(moves[i])];
 		
+		if (!IS_EMPTY(cap))		values[i] = cap - board.squares[MOVE_GET_FROM(moves[i])];
+		else					values[i] = -board.squares[MOVE_GET_FROM(moves[i])];
+		
+		int sq64 = CONVERT_256_TO_64(MOVE_GET_TO(moves[i]));
+		int truesq = board.turn == ColourWhite ? sq64 : inv[sq64];
+		int is_endgame  = (board.piece_counts[0] + board.piece_counts[1] <= 7);
+		
+		switch(board.squares[MOVE_GET_FROM(moves[i])] & ~1){
+			case WhitePawn:
+				if (is_endgame) values[i] += PawnEndValueMap[truesq];
+				else			values[i] += PawnEarlyValueMap[truesq];
+			
+			case WhiteKnight:
+				values[i] += KnightValueMap[truesq];
+			
+			case WhiteBishop:
+				values[i] += BishopValueMap[truesq];
+		}
+			
 		if (MOVE_IS_PROMOTION(moves[i]))
 			values[i] += 50 * MOVE_GET_PROMOTE_TYPE(moves[i],ColourWhite);
 		
@@ -311,12 +289,24 @@ void basic_heuristic(move_t * moves, int size){
 		else if (arr[0] == moves[i])
 			values[i] += 25000;
 		else if (arr[1] == moves[i])
-			values[i] += 20000;
+			values[i] += 25000;
 		else if (arr[2] == moves[i])
-			values[i] += 15000;
-		else if (arr[3] == moves[i])
-			values[i] += 10000;
-		else if (arr[4] == moves[i])
+			values[i] += 25000;
+	}
+	
+	order_by_value(moves,values,size);
+}
+
+void quiescence_heuristic(move_t * moves, int size){
+	move_t * arr = &(tree.killer_moves[tree.ply][0]);
+	int i, values[size];
+	for(i = 0; i < size; i++){
+		values[i] =  40 * MOVE_GET_CAPTURE(moves[i]) - board.squares[MOVE_GET_FROM(moves[i])];
+		if (arr[0] == moves[i])
+			values[i] += 5000;
+		else if (arr[1] == moves[i])
+			values[i] += 5000;
+		else if (arr[2] == moves[i])
 			values[i] += 5000;
 	}
 	
@@ -325,9 +315,10 @@ void basic_heuristic(move_t * moves, int size){
 
 void update_killer_heuristic(move_t move){
 	move_t * arr = &(tree.killer_moves[tree.ply][0]);
-	arr[4] = arr[3];
-	arr[3] = arr[2];
-	arr[2] = arr[1];
-	arr[1] = arr[0];
-	arr[0] = move;
+	
+	if (move != arr[2] && move != arr[1] && move != arr[0]){
+		arr[2] = arr[1];
+		arr[1] = arr[0];
+		arr[0] = move;
+	}
 }
