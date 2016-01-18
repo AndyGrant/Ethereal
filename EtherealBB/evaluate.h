2ee39d32ace33b7f23115f69b0e7b47c2a86a258
AndyGrant <Andrew@grantnet.us>
2016-01-17 23:16:55 -0500

    Added Material Values to Evaluate.h. Added all PSQT entries based off of stockfish7's evaluation tables. (May need to adjust King Table).

---
+++ EtherealBB/evaluate.h
@@ -0,0 +1,11 @@
+#ifndef _EVALUATE_H
+#define _EVALUATE_H
+
+#define PawnValue	(100)
+#define KnightValue	(300)
+#define BishopValue	(300)
+#define RookValue	(500)
+#define QueenValue	(900)
+#define KingValue	(  0)
+
+#endif
--- EtherealBB/main.c
+++ EtherealBB/main.c
@@ -16,12 +16,4 @@
 int main(){
 	move_gen_test();
 	return 0;
-	
-	Board board;	
-	init_board(&board,"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");	
-	printf("Opening=%d Endgame=%d\n",board.opening,board.endgame);
-	print_board(&board);
-	
-	
-	//printf("Moves : %d\n",perft(&board,6));
 }
