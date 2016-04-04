3129d1fa813e971732ebf9d05d5e652a116595a4
AndrewGrant <andrew@grantnet.us>
2016-04-04 15:11:02 -0400

    Added stable quiescence search, increased heuristic evals in sort_moves. Added null move windows to alpha_beta_prune.

--- EtherealBB/search.h
+++ EtherealBB/search.h
@@ -11,6 +11,9 @@
 /* Prototypes */
 uint16_t get_best_move(Board * board, int seconds);
 int alpha_beta_prune(Board * board, int alpha, int beta, int depth, int height);
-void sort_moves(uint16_t * moves, int size, int depth, int height, TranspositionEntry * entry);
+int quiescence_search(Board * board, int alpha, int beta, int height);
+void sort_moves(Board * board, uint16_t * moves, int size, int depth, int height, TranspositionEntry * entry);
+int evaluate_board(Board * board);
+
 
 #endif
