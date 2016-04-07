caead2afae2c6755e706629550589ab1862acbac
AndyGrant <Andrew@grantnet.us>
2016-04-07 00:07:01 -0400

    .

--- EtherealBB/search.h
+++ EtherealBB/search.h
@@ -10,7 +10,7 @@
 
 /* Prototypes */
 uint16_t get_best_move(Board * board, int seconds);
-int alpha_beta_prune(Board * board, int alpha, int beta, int depth, int height);
+int alpha_beta_prune(Board * board, int alpha, int beta, int depth, int height, int node_type);
 int quiescence_search(Board * board, int alpha, int beta, int height);
 void sort_moves(Board * board, uint16_t * moves, int size, int depth, int height, uint16_t best_move);
 int evaluate_board(Board * board);
