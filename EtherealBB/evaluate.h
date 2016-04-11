a4acc193c394dac0a6c7c96188c07abd51ba0371
AndyGrant <Andrew@grantnet.us>
2016-04-10 23:46:21 -0400

    Removed passedpawns, tweaked sort_moves to more activly find moves which could cause cut-offcut-offs

--- EtherealBB/evaluate.h
+++ EtherealBB/evaluate.h
@@ -12,7 +12,6 @@
 
 #define PAWN_STACKED_PENALTY  ( 30)
 #define PAWN_ISOLATED_PENALTY ( 20)
-#define PAWN_PASSED_BONUS     ( 15)
 #define ROOK_7TH_RANK_VALUE   ( 35)
 #define ROOK_8TH_RANK_VALUE   ( 45)
 
