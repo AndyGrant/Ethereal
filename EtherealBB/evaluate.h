f879a84d55836a2f2897eee7d7b1d023890ca9c7
AndrewGrant <andrew@grantnet.us>
2016-04-13 12:43:01 -0400

    Overhaul of evaluation

--- EtherealBB/evaluate.h
+++ EtherealBB/evaluate.h
@@ -5,17 +5,23 @@
 
 #define PawnValue   ( 100)
 #define KnightValue ( 300)
-#define BishopValue ( 300)
+#define BishopValue ( 310)
 #define RookValue   ( 500)
-#define QueenValue  ( 900)
+#define QueenValue  ( 925)
 #define KingValue   (  50)
 
-#define PAWN_STACKED_PENALTY   ( 30)
-#define PAWN_ISOLATED_PENALTY  ( 20)
-#define PAWN_7TH_RANK_VALUE    ( 95)
-
-#define ROOK_7TH_RANK_VALUE    ( 35)
-#define ROOK_8TH_RANK_VALUE    ( 45)
+#define PAWN_STACKED_PENALTY   	( 45)
+#define PAWN_ISOLATED_PENALTY  	( 20)
+#define PAWN_7TH_RANK_VALUE    	( 95)
+	
+#define ROOK_7TH_RANK_VALUE    	( 35)
+#define ROOK_8TH_RANK_VALUE    	( 40)
+#define ROOK_ON_SAME_FILE_VALUE	( 65)
+	
+#define KNIGHT_ATTACK_VALUE	   	( 13)
+#define KNIGHT_DEFEND_VALUE	   	(  7)
+	
+#define PAWN_DEFEND_PAWN_VALUE 	(  9)
 
 static int PieceValues[8] = {PawnValue, KnightValue, BishopValue, RookValue, QueenValue, KingValue, 0, 0};
 
