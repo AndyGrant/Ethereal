158c42b4ce325b3c7a97be0c811d048415eb9e06
AndyGrant <Andrew@grantnet.us>
2016-04-16 04:55:04 -0400

    Re-wrote entire search.c...

--- EtherealBB/search.h
+++ EtherealBB/search.h
@@ -9,12 +9,13 @@
 #define MaxKillers  (3)
 
 /* Prototypes */
-uint16_t get_best_move(Board * board, int seconds, int send_results);
-int alpha_beta_prune(Board * board, int alpha, int beta, int depth, int height, int node_type);
-int quiescence_search(Board * board, int alpha, int beta, int height);
-void heuristic_eval_moves(Board * board, int * values, uint16_t * moves, int size, int height, uint16_t best_move);
-uint16_t get_next_move(uint16_t * moves, int * values, int index, int size);
-int evaluate_board(Board * board);
 
+uint16_t get_best_move(Board * board, int seconds, int logging);
+int full_search(Board * board, MoveList * moveList, int depth);
+int search(Board * board, int alpha, int beta, int depth, int height, int node_type);
+int qsearch(Board * board, int alpha, int beta, int height);
+void evaluate_moves(Board * board, int * values, uint16_t * moves, int size, int height, uint16_t tableMove);
+uint16_t get_next_move(uint16_t * moves, int * values, int index, int size);
+void sort_move_list(MoveList * moveList);
 
 #endif
