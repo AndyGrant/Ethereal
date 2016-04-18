b1a8b7c1cb60792623f7d5d12dc5f61f171816f0
AndyGrant <Andrew@grantnet.us>
2016-04-18 14:57:36 -0400

    Changed values of pieces to match Fruit2.1. Modified values of various bonuses to more closely match actual values?

--- EtherealBB/evaluate.h
+++ EtherealBB/evaluate.h
@@ -3,31 +3,31 @@
 
 #include "types.h"
 
-#define PawnValue   ( 100)
-#define KnightValue ( 300)
-#define BishopValue ( 300)
-#define RookValue   ( 500)
-#define QueenValue  ( 900)
-#define KingValue   (  50)
+#define PawnValue   (  100)
+#define KnightValue (  400)
+#define BishopValue (  400)
+#define RookValue   (  600)
+#define QueenValue  ( 1200)
+#define KingValue   (   50)
 
-#define PAWN_STACKED_MID     (20)
-#define PAWN_STACKED_END     (15)
-#define PAWN_ISOLATED_MID    (15)
+#define PAWN_STACKED_MID     (15)
+#define PAWN_STACKED_END     (35)
+#define PAWN_ISOLATED_MID    (20)
 #define PAWN_ISOLATED_END    (10)
-#define PAWN_7TH_RANK_MID    (25)
-#define PAWN_7TH_RANK_END    (45)
+#define PAWN_7TH_RANK_MID    (35)
+#define PAWN_7TH_RANK_END    (55)
 
-#define ROOK_OPEN_FILE_MID   (45)
+#define ROOK_OPEN_FILE_MID   (55)
 #define ROOK_OPEN_FILE_END   (45)
-#define ROOK_SEMI_FILE_MID   (15)
-#define ROOK_SEMI_FILE_END   (15)
-#define ROOK_ON_7TH_MID      (25)
+#define ROOK_SEMI_FILE_MID   (25)
+#define ROOK_SEMI_FILE_END   (25)
+#define ROOK_ON_7TH_MID      (35)
 #define ROOK_ON_7TH_END      (15)
 
-#define BISHOP_PAIR_MID      (12)
-#define BISHOP_PAIR_END      (24)
-#define BISHOP_HAS_WINGS_MID ( 8)
-#define BISHOP_HAS_WINGS_END (24)
+#define BISHOP_PAIR_MID      (32)
+#define BISHOP_PAIR_END      (64)
+#define BISHOP_HAS_WINGS_MID (18)
+#define BISHOP_HAS_WINGS_END (42)
 
 static int PieceValues[8] = {PawnValue, KnightValue, BishopValue, RookValue, QueenValue, KingValue, 0, 0};
 
