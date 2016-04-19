aea069056eee60e44541354f47d22237767f4b06
AndyGrant <Andrew@grantnet.us>
2016-04-18 22:03:21 -0400

    Added eval for passed pawns. Cleaned up much of evaluate.c. Added util for finding MSB in a special case to bitutils.c

--- EtherealBB/bitutils.c
+++ EtherealBB/bitutils.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
+#include <assert.h>
 
+#include "bitboards.h"
 #include "bitutils.h"
+
 
 /*
  *  Determine number of set bits in a given 
@@ -32,5 +35,21 @@ void get_set_bits(uint64_t bb, int * arr){
     }
     
     arr[count] = -1;    
+}
+
+/*
+ * Determine the Most Significant Bit of a BitBoard
+ *
+ * This can ONLY be used when we know that
+ * all bits will be in the same column, (FILE)
+ */
+int get_msb_special(uint64_t bb){
+    if (bb & RANK_7) return 48;
+    if (bb & RANK_6) return 40;
+    if (bb & RANK_5) return 32;
+    if (bb & RANK_4) return 24;
+    if (bb & RANK_3) return 16;
+    if (bb & RANK_2) return  8;
+    assert("Invalid uint64_t send!\n" && 0);
 }
 
--- EtherealBB/evaluate.c
+++ EtherealBB/evaluate.c
@@ -15,16 +15,60 @@ int evaluate_board(Board * board){
     int i, num, mid = 0, end = 0;
     int curPhase, mid_eval, end_eval, eval;
     
+    // EXTRACT BASIC BITBOARDS FROM BOARD
     uint64_t white    = board->colourBitBoards[ColourWhite];
     uint64_t black    = board->colourBitBoards[ColourBlack];
-    
     uint64_t pawns    = board->pieceBitBoards[0];
     uint64_t knights  = board->pieceBitBoards[1];
     uint64_t bishops  = board->pieceBitBoards[2];
     uint64_t rooks    = board->pieceBitBoards[3];
     uint64_t queens   = board->pieceBitBoards[4];
     
-    // Check for Recognized Draws
+    // CREATE BITBOARD FOR PIECES OF EACH COLOUR
+    uint64_t whitePawns   = white & pawns;
+    uint64_t blackPawns   = black & pawns;
+    uint64_t whiteBishops = white & bishops;
+    uint64_t blackBishops = black & bishops;
+    uint64_t whiteRooks   = white & rooks;
+    uint64_t blackRooks   = black & rooks;
+
+    // CREATE BITBOARD FOR EACH FILE, OF EACH COLOUR, FOR PAWNS
+    uint64_t wa = whitePawns & FILE_A, wb = whitePawns & FILE_B;
+    uint64_t wc = whitePawns & FILE_C, wd = whitePawns & FILE_D;
+    uint64_t we = whitePawns & FILE_E, wf = whitePawns & FILE_F;
+    uint64_t wg = whitePawns & FILE_G, wh = whitePawns & FILE_H;
+    uint64_t ba = blackPawns & FILE_A, bb = blackPawns & FILE_B;
+    uint64_t bc = blackPawns & FILE_C, bd = blackPawns & FILE_D;
+    uint64_t be = blackPawns & FILE_E, bf = blackPawns & FILE_F;
+    uint64_t bg = blackPawns & FILE_G, bh = blackPawns & FILE_H;
+    
+    // GET LSB FOR EACH FILE OF PAWNS
+    int lwa = get_lsb(wa), lwb = get_lsb(wb);
+    int lwc = get_lsb(wc), lwd = get_lsb(wd);
+    int lwe = get_lsb(we), lwf = get_lsb(wf);
+    int lwg = get_lsb(wg), lwh = get_lsb(wh);
+    
+    int lba = get_lsb(ba), lbb = get_lsb(bb);
+    int lbc = get_lsb(bc), lbd = get_lsb(bd);
+    int lbe = get_lsb(be), lbf = get_lsb(bf);
+    int lbg = get_lsb(bg), lbh = get_lsb(bh);
+    
+    // FOR PAWN EVALUATIONS
+    int pawnIsStacked[2][8];
+    int pawnIsIsolated[2][8];
+    int pawnIsPassed[2][8];
+    
+    // GET INDEXES OF EACH COLOUR'S ROOKS
+    int wrooks[16], brooks[16];
+    get_set_bits(whiteRooks, wrooks);
+    get_set_bits(blackRooks, brooks);
+    
+    // GET INDEXES OF EACH COLOUR'S BISHOPS
+    int wbishops[16], bbishops[16];
+    get_set_bits(whiteBishops, wbishops);
+    get_set_bits(blackBishops, bbishops);
+    
+    // CHECK FOR RECOGNIZED DRAWS
     if (pawns == 0 && rooks == 0 && queens == 0){
         
         // K v K
@@ -58,97 +102,103 @@ int evaluate_board(Board * board){
             count_set_bits(black & knights) == 2 &&
             count_set_bits(black & bishops) == 0)
             return 0;
+    }    
+    
+    // DETERMINE IF STACKED PAWN ON GIVEN FILE
+    pawnIsStacked[0][0] = ((wa & (wa-1)) != 0);
+    pawnIsStacked[0][1] = ((wb & (wb-1)) != 0);
+    pawnIsStacked[0][2] = ((wc & (wc-1)) != 0);
+    pawnIsStacked[0][3] = ((wd & (wd-1)) != 0);
+    pawnIsStacked[0][4] = ((we & (we-1)) != 0);
+    pawnIsStacked[0][5] = ((wf & (wf-1)) != 0);
+    pawnIsStacked[0][6] = ((wg & (wg-1)) != 0);
+    pawnIsStacked[0][7] = ((wh & (wh-1)) != 0);
+    pawnIsStacked[1][0] = ((ba & (ba-1)) != 0);
+    pawnIsStacked[1][1] = ((bb & (bb-1)) != 0);
+    pawnIsStacked[1][2] = ((bc & (bc-1)) != 0);
+    pawnIsStacked[1][3] = ((bd & (bd-1)) != 0);
+    pawnIsStacked[1][4] = ((be & (be-1)) != 0);
+    pawnIsStacked[1][5] = ((bf & (bf-1)) != 0);
+    pawnIsStacked[1][6] = ((bg & (bg-1)) != 0);
+    pawnIsStacked[1][7] = ((bh & (bh-1)) != 0);
+    
+    // DETERMINE IF ISOLATED PAWN ON GIVEN FILE
+    pawnIsIsolated[0][0] = (wa && !wb       );
+    pawnIsIsolated[0][1] = (wb && !wa && !wc);
+    pawnIsIsolated[0][2] = (wc && !wb && !wd);
+    pawnIsIsolated[0][3] = (wd && !wc && !we);
+    pawnIsIsolated[0][4] = (we && !wd && !wf);
+    pawnIsIsolated[0][5] = (wf && !we && !wg);
+    pawnIsIsolated[0][6] = (wg && !wf && !wh);
+    pawnIsIsolated[0][7] = (wh && !wg       );
+    pawnIsIsolated[1][0] = (ba && !bb       );
+    pawnIsIsolated[1][1] = (bb && !ba && !bc);
+    pawnIsIsolated[1][2] = (bc && !bb && !bd);
+    pawnIsIsolated[1][3] = (bd && !bc && !be);
+    pawnIsIsolated[1][4] = (be && !bd && !bf);
+    pawnIsIsolated[1][5] = (bf && !be && !bg);
+    pawnIsIsolated[1][6] = (bg && !bf && !bh);
+    pawnIsIsolated[1][7] = (bh && !bg       );
+    
+    // DETERMINE IF PASSED PAWN ON GIVEN FILE
+    pawnIsPassed[0][0] = (wa > ba) && (wa << 1 >= bb)                   ;
+    pawnIsPassed[0][1] = (wb > bb) && (wb << 1 >= bc) && (wb >> 1 >= ba);
+    pawnIsPassed[0][2] = (wc > bc) && (wc << 1 >= bd) && (wc >> 1 >= bb);
+    pawnIsPassed[0][3] = (wd > bd) && (wd << 1 >= be) && (wd >> 1 >= bc);
+    pawnIsPassed[0][4] = (we > be) && (we << 1 >= bf) && (we >> 1 >= bd);
+    pawnIsPassed[0][5] = (wf > bf) && (wf << 1 >= bg) && (wf >> 1 >= be);
+    pawnIsPassed[0][6] = (wg > bg) && (wg << 1 >= bh) && (wg >> 1 >= bf);
+    pawnIsPassed[0][7] = (wh > bh)                    && (wh >> 1 >= bg);
+    pawnIsPassed[1][0] = (lba < lwa) && (lba + 1 <= lwb)                    ;
+    pawnIsPassed[1][1] = (lbb < lwb) && (lbb + 1 <= lwc) && (lbb - 1 <= lwa);
+    pawnIsPassed[1][2] = (lbc < lwc) && (lbc + 1 <= lwd) && (lbc - 1 <= lwb);
+    pawnIsPassed[1][3] = (lbd < lwd) && (lbd + 1 <= lwe) && (lbd - 1 <= lwc);
+    pawnIsPassed[1][4] = (lbe < lwe) && (lbe + 1 <= lwf) && (lbe - 1 <= lwd);
+    pawnIsPassed[1][5] = (lbf < lwf) && (lbf + 1 <= lwg) && (lbf - 1 <= lwe);
+    pawnIsPassed[1][6] = (lbg < lwg) && (lbg + 1 <= lwh) && (lbg - 1 <= lwf);
+    pawnIsPassed[1][7] = (lbh < lwh)                     && (lbh - 1 <= lwg);
+    
+    // APPLY BONUSES AND PENALTIES FOR WHITE'S PAWN STRUCTURE
+    for (i = 0; i < 8; i++){
+        if (pawnIsStacked[0][i]){
+            mid -= PawnStackedMid[i];
+            end -= PawnStackedEnd[i];
+        }
+        
+        if (pawnIsIsolated[0][i]){
+            mid -= PawnIsolatedMid[i];
+            end -= PawnIsolatedEnd[i];
+        }
+        
+        if (pawnIsPassed[0][i]){
+            int msb = get_msb_special(whitePawns & FILES[i]);
+            mid += PawnPassedMid[msb>>3];
+            end += PawnPassedEnd[msb>>3];
+        }
     }
     
-    uint64_t whitePawns = white & pawns;
-    uint64_t blackPawns = black & pawns;
     
-    uint64_t whiteBishops = white & bishops;
-    uint64_t blackBishops = black & bishops;
-    
-    uint64_t whiteRooks = white & rooks;
-    uint64_t blackRooks = black & rooks;
-    
-    uint64_t wa = whitePawns & FILE_A, wb = whitePawns & FILE_B;
-    uint64_t wc = whitePawns & FILE_C, wd = whitePawns & FILE_D;
-    uint64_t we = whitePawns & FILE_E, wf = whitePawns & FILE_F;
-    uint64_t wg = whitePawns & FILE_G, wh = whitePawns & FILE_H;
-    
-    uint64_t ba = blackPawns & FILE_A, bb = blackPawns & FILE_B;
-    uint64_t bc = blackPawns & FILE_C, bd = blackPawns & FILE_D;
-    uint64_t be = blackPawns & FILE_E, bf = blackPawns & FILE_F;
-    uint64_t bg = blackPawns & FILE_G, bh = blackPawns & FILE_H;
-    
-    
-    
-    // STACKED PAWNS
-    int net_stacked = (
-        ((wa & (wa-1)) != 0) +
-        ((wb & (wb-1)) != 0) +
-        ((wc & (wc-1)) != 0) +
-        ((wd & (wd-1)) != 0) +
-        ((we & (we-1)) != 0) +
-        ((wf & (wf-1)) != 0) +
-        ((wg & (wg-1)) != 0) +
-        ((wh & (wh-1)) != 0) -
-    
-        ((ba & (ba-1)) != 0) -
-        ((bb & (bb-1)) != 0) -
-        ((bc & (bc-1)) != 0) -
-        ((bd & (bd-1)) != 0) -
-        ((be & (be-1)) != 0) -
-        ((bf & (bf-1)) != 0) -
-        ((bg & (bg-1)) != 0) -
-        ((bh & (bh-1)) != 0)
-    );
-    
-    mid -= net_stacked * PAWN_STACKED_MID;
-    end -= net_stacked * PAWN_STACKED_END;
-    
-    
-    // ISOLATED PAWNS
-    int net_isolated = (
-        (wa && !wb       ) +
-        (wb && !wa && !wc) +
-        (wc && !wb && !wd) +
-        (wd && !wc && !we) +
-        (we && !wd && !wf) +
-        (wf && !we && !wg) +
-        (wg && !wf && !wh) +
-        (wh && !wg       ) -
+    // APPLY BONUSES AND PENALTIES FOR BLACK'S PAWN STRUCTURE
+    for (i = 0; i < 8; i++){
+        if (pawnIsStacked[1][i]){
+            mid += PawnStackedMid[i];
+            end += PawnStackedEnd[i];
+        }
         
-        (ba && !bb       ) -
-        (bb && !ba && !bc) -
-        (bc && !bb && !bd) -
-        (bd && !bc && !be) -
-        (be && !bd && !bf) -
-        (bf && !be && !bg) -
-        (bg && !bf && !bh) -
-        (bh && !bg       )
-    );
+        if (pawnIsIsolated[1][i]){
+            mid += PawnIsolatedMid[i];
+            end += PawnIsolatedEnd[i];
+        }
+        
+        if (pawnIsPassed[1][i]){
+            int lsb = get_lsb(blackPawns & FILES[i]);
+            mid -= PawnPassedMid[7-(lsb>>3)];
+            end -= PawnPassedEnd[7-(lsb>>3)];
+        }
+    }
     
-    mid -= net_isolated * PAWN_ISOLATED_MID;
-    end -= net_isolated * PAWN_ISOLATED_END;
-    
-    int net_7th = (
-        count_set_bits(whitePawns & RANK_7) -
-        count_set_bits(blackPawns & RANK_2)
-    );
-    
-    mid += net_7th * PAWN_7TH_RANK_MID;
-    end += net_7th * PAWN_7TH_RANK_END;
-    
-    /* PASSED PAWNS */
-    /* BACKWARDS PAWNS */    
-    
-    
-    int wrooks[16], brooks[16];
-    get_set_bits(whiteRooks, wrooks);
-    get_set_bits(blackRooks, brooks);
-    
+    // EVALUATE WHITE ROOKS FOR FILE AND RANK BONUSES
     for (i = 0; wrooks[i] != -1; i++){
-        
-        // Rooks Open File and Semi Open file
         if (whitePawns & FILES[wrooks[i] % 8] == 0){
             if (blackPawns & FILES[wrooks[i] % 8] == 0){
                 mid += ROOK_OPEN_FILE_MID;
@@ -160,16 +210,14 @@ int evaluate_board(Board * board){
             }
         }
         
-        // Rook on 7th
         if ((wrooks[i] >> 3) == 6){
             mid += ROOK_ON_7TH_MID;
             end += ROOK_ON_7TH_END;
         }
     }
     
+    // EVALUATE BLACK ROOKS FOR FILE AND RANK BONUSES
     for (i = 0; brooks[i] != -1; i++){
-        
-        // Rooks Open File and Semi Open file
         if (blackPawns & FILES[brooks[i] % 8] == 0){
             if (whitePawns & FILES[brooks[i] % 8] == 0){
                 mid -= ROOK_OPEN_FILE_MID;
@@ -181,7 +229,6 @@ int evaluate_board(Board * board){
             }
         }
         
-        // Rook on 7th
         if ((brooks[i] >> 3) == 1){
             mid -= ROOK_ON_7TH_MID;
             end -= ROOK_ON_7TH_END;
@@ -189,29 +236,28 @@ int evaluate_board(Board * board){
     }
     
     
-    int wbishops[16], bbishops[16];
-    get_set_bits(whiteBishops, wbishops);
-    get_set_bits(blackBishops, bbishops);
-    
-    // Bishop Pair
+    // WHITE HAS A BISHOP PAIR
     if (wbishops[0] != -1 && wbishops[1] != -1){
         mid += BISHOP_PAIR_MID;
         end += BISHOP_PAIR_END;
     }
+    
+    // BLACK HAS A BISHOP PAIR
     if (bbishops[0] != -1 && bbishops[1] != -1){
         mid -= BISHOP_PAIR_MID;
         end -= BISHOP_PAIR_END;
     }
     
-    // Bishop has Pawn Wings
+    // WHITE HAS A SOLE BISHOP WITH WINGED PAWNS
     if (whiteBishops != 0 && 
        (whitePawns & (FILE_A|FILE_B)) != 0 &&
        (whitePawns & (FILE_G|FILE_H)) != 0){
-       
+           
        mid += BISHOP_HAS_WINGS_MID;
        end += BISHOP_HAS_WINGS_END;
     }
-       
+    
+    // BLACK HAS A SOLE BISHOP WITH WINGED PAWNS
     if (blackBishops != 0 && 
        (blackPawns & (FILE_A|FILE_B)) != 0 &&
        (blackPawns & (FILE_G|FILE_H)) != 0){
@@ -230,5 +276,5 @@ int evaluate_board(Board * board){
     
     eval = ((mid_eval * (256 - curPhase)) + (end_eval * curPhase)) / 256;
     
-    return board->turn == ColourWhite ? eval+20 : -(eval+10);    
+    return board->turn == ColourWhite ? eval+10 : -(eval+10);    
 }
