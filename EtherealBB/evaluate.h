072f8db75f978d38468895b7ff28dc81739c479a
AndyGrant <Andrew@grantnet.us>
2016-04-26 02:28:08 -0400

    I keep increasing the value of passed pawns. Engine refuses to deal with it... algorithm wrong??

--- EtherealBB/evaluate.h
+++ EtherealBB/evaluate.h
@@ -21,17 +21,17 @@
 
 #define BISHOP_PAIR_MID      (13)
 #define BISHOP_PAIR_END      (29)
-#define BISHOP_HAS_WINGS_MID ( 7)
+#define BISHOP_HAS_WINGS_MID (13)
 #define BISHOP_HAS_WINGS_END (32)
 
-static int PawnStackedMid[8]  = { 5,  9, 12, 13, 13, 12,  9,  5};
+static int PawnStackedMid[8]  = {10, 14, 17, 18, 18, 17, 14, 10};
 static int PawnStackedEnd[8]  = {15, 19, 22, 23, 23, 22, 19, 15};
 
 static int PawnIsolatedMid[8] = {14, 20, 26, 26, 26, 26, 20, 14};
 static int PawnIsolatedEnd[8] = {10, 16, 18, 18, 18, 18, 16, 10};
 
-static int PawnPassedMid[8]   = { 0,  0,  0, 10, 15, 24, 33,  0};
-static int PawnPassedEnd[8]   = { 0,  0,  0, 10, 15, 25, 33,  0};
+static int PawnPassedMid[8]   = { 0,  0,  0, 12, 17, 27, 35,  0};
+static int PawnPassedEnd[8]   = { 0,  0,  0, 12, 17, 27, 35,  0};
 
 static int PieceValues[8] = {PawnValue, KnightValue, BishopValue, RookValue, QueenValue, KingValue, 0, 0};
 
