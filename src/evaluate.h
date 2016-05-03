7d5549a2aedeabc1b47468797c2587e511c20e2f
AndyGrant <Andrew@grantnet.us>
2016-05-03 02:02:00 -0400

    Updated some evaluation values

--- src/evaluate.h
+++ src/evaluate.h
@@ -10,15 +10,17 @@
 #define QueenValue  ( 1200)
 #define KingValue   (   50)
 
-#define ROOK_OPEN_FILE_MID   (25)
-#define ROOK_OPEN_FILE_END   (15)
-#define ROOK_SEMI_FILE_MID   ( 8)
-#define ROOK_SEMI_FILE_END   ( 8)
+#define ROOK_OPEN_FILE_MID   (27)
+#define ROOK_OPEN_FILE_END   (17)
+#define ROOK_SEMI_FILE_MID   (12)
+#define ROOK_SEMI_FILE_END   (12)
 #define ROOK_ON_7TH_MID      (15)
 #define ROOK_ON_7TH_END      ( 6)
+#define ROOK_STACKED_MID     (19)
+#define ROOK_STACKED_END     (19)
 
-#define BISHOP_PAIR_MID      ( 9)
-#define BISHOP_PAIR_END      (17)
+#define BISHOP_PAIR_MID      (36)
+#define BISHOP_PAIR_END      (63)
 #define BISHOP_HAS_WINGS_MID ( 7)
 #define BISHOP_HAS_WINGS_END (32)
 
@@ -28,8 +30,8 @@ static int PawnStackedEnd[8]  = {15, 19, 22, 23, 23, 22, 19, 15};
 static int PawnIsolatedMid[8] = {14, 20, 26, 26, 26, 26, 20, 14};
 static int PawnIsolatedEnd[8] = {10, 16, 18, 18, 18, 18, 16, 10};
 
-static int PawnPassedMid[8]   = { 0,  0,  0,  8, 12, 20, 28,  0};
-static int PawnPassedEnd[8]   = { 0,  0,  0,  8, 12, 20, 28,  0};
+static int PawnPassedMid[8]   = { 0,  0,  0, 10, 15, 24, 33,  0};
+static int PawnPassedEnd[8]   = { 0,  0,  0, 10, 15, 24, 33,  0};
 
 static int PieceValues[8] = {PawnValue, KnightValue, BishopValue, RookValue, QueenValue, KingValue, 0, 0};
 
