8c2032dcf1a72f471ab46136238081a4e50e012a
AndyGrant <Andrew@grantnet.us>
2016-05-07 03:24:15 -0400

    .

--- src/evaluate.h
+++ src/evaluate.h
@@ -10,18 +10,18 @@
 #define QueenValue  ( 2400)
 #define KingValue   (   50)
 
-#define ROOK_OPEN_FILE_MID   (27)
-#define ROOK_OPEN_FILE_END   (17)
+#define ROOK_OPEN_FILE_MID   (22)
+#define ROOK_OPEN_FILE_END   (15)
 #define ROOK_SEMI_FILE_MID   (12)
 #define ROOK_SEMI_FILE_END   (12)
 #define ROOK_ON_7TH_MID      (15)
 #define ROOK_ON_7TH_END      ( 6)
-#define ROOK_STACKED_MID     (19)
-#define ROOK_STACKED_END     (19)
+#define ROOK_STACKED_MID     (15)
+#define ROOK_STACKED_END     (15)
 
 #define BISHOP_PAIR_MID      (36)
 #define BISHOP_PAIR_END      (63)
-#define BISHOP_HAS_WINGS_MID ( 7)
+#define BISHOP_HAS_WINGS_MID (17)
 #define BISHOP_HAS_WINGS_END (32)
 
 static int PawnStackedMid[8]  = { 5,  9, 12, 13, 13, 12,  9,  5};
--- src/transposition.c
+++ src/transposition.c
@@ -36,51 +36,40 @@ TransEntry * getTranspositionEntry(TransTable * table, uint64_t hash){
 }
 
 void storeTranspositionEntry(TransTable * table, int depth, int turn, int type, int value, uint16_t bestMove, uint64_t hash){
-    
     TransBucket * bucket = &(table->buckets[hash % table->maxSize]);
     TransEntry * toReplace = NULL;
-    TransEntry * candidate;
-    int i, j;
-    
-    // SEARCH FOR ENTRY WITH MATCHING HASH
+    int i;
+
     for (i = 0; i < 4; i++){
         if (bucket->entries[i].hash == hash){
-            if (depth >= bucket->entries[i].depth){
+            if (depth >= bucket->entries[i].depth)
                 toReplace = &(bucket->entries[i]);
-                goto Replace;
-            }
-            
-            candidate = &(bucket->entries[i]);
-            candidate->info = (table->age << 3) | (candidate->info & 0b111);
-            return;
+            else
+                return;
+            goto Replace;
         }
     }
     
-    // SEARCH FOR LOWEST DRAFT + OLD ENTRY
-    toReplace = &(bucket->entries[3]);
-    for (i = 2; i >= 0; i--){
-        if (EntryAge(&(bucket->entries[i])) != table->age
-            && bucket->entries[i].depth < toReplace->depth)
-            toReplace = &(bucket->entries[i]);
+    for (i = 0; i  < 4; i++){
+        if ((bucket->entries[i].info >> 3) != table->age){
+            if (toReplace == NULL
+                || toReplace->depth > bucket->entries[i].depth)
+                toReplace = &(bucket->entries[i]);
+        }
     }
-    
-    // OLD ENTRY FOUND?
-    if (EntryAge(toReplace) != table->age)
+    if (toReplace != NULL)
         goto Replace;
     
-    
-    // NO OLD OR MATCHING ENTRY FOUND, REPLACE LOWEST DRAFT
-    toReplace = &(bucket->entries[3]);
-    for (i = 2; i >= 0; i--){
+    toReplace = &(bucket->entries[0]);
+    for (i = 1; i < 4; i++){
         if (bucket->entries[i].depth < toReplace->depth)
             toReplace = &(bucket->entries[i]);
     }
     
-    // PERFORM THE REPLACEMENT
-    Replace:
-        toReplace->depth = (uint8_t)depth;
-        toReplace->info  = (uint8_t)((table->age << 3) | (type << 1) | (turn));
-        toReplace->value = (int16_t)value;
+    Replace: // PERFORM THE REPLACEMENT
+        toReplace->depth = depth;
+        toReplace->info  = (table->age << 3) | (type << 1) | (turn);
+        toReplace->value = value;
         toReplace->best  = bestMove;
         toReplace->hash  = hash;
 }
