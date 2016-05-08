063de928b33ba1c1c38682c4e0d4d0fca67f87fe
AndyGrant <Andrew@grantnet.us>
2016-05-08 17:01:36 -0400

    Fixing evaluations of OpenFile and SemiOpenFile rooks, as well as counting of null moves in the board history, have caused the skill level of the engine to drop. Commit#635 took 64% vs MicroMax, but the most recent commit struggles to push 55%. As far as I know, fixing the null move counter should not have caused the engine to worsen. It 'should' have only fixed very rare cases. Additionally, moving the repition checking torwards the top of the search should have made repititions appear to the search much earlier. So, the issue must have been with the rook evaluations. Previusly the rooks were never evaluated, so now it only makes sense that my current evaluations are too valued. I have reverted back to the piece values of C635, increased evaluations for the pawns, and drastically reduced evaluations for the rooks. Additionally, I have lowered the validCount and depth requirments for LMR, but will on longer do LMR on pawn moves.

--- src/evaluate.h
+++ src/evaluate.h
@@ -3,24 +3,27 @@
 
 #include "types.h"
 
-#define PawnValue   (  100)
-#define KnightValue (  400)
-#define BishopValue (  400)
-#define RookValue   (  600)
-#define QueenValue  ( 1200)
+#define PawnValue   (  200)
+#define KnightValue (  800)
+#define BishopValue (  800)
+#define RookValue   ( 1200)
+#define QueenValue  ( 2400)
 #define KingValue   (   50)
 
-#define ROOK_OPEN_FILE_MID   (35)
-#define ROOK_OPEN_FILE_END   (25)
+#define TEMPO_MID   (14)
+#define TEMPO_END   (10)
 
-#define ROOK_SEMI_FILE_MID   (10)
-#define ROOK_SEMI_FILE_END   (10)
+#define ROOK_OPEN_FILE_MID   (15)
+#define ROOK_OPEN_FILE_END   (10)
 
-#define ROOK_ON_7TH_MID      (25)
-#define ROOK_ON_7TH_END      (30)
+#define ROOK_SEMI_FILE_MID   ( 9)
+#define ROOK_SEMI_FILE_END   ( 9)
 
-#define ROOK_STACKED_MID     ( 8)
-#define ROOK_STACKED_END     (10)
+#define ROOK_ON_7TH_MID      (10)
+#define ROOK_ON_7TH_END      (15)
+
+#define ROOK_STACKED_MID     ( 6)
+#define ROOK_STACKED_END     ( 6)
 
 #define BISHOP_PAIR_MID      (36)
 #define BISHOP_PAIR_END      (48)
@@ -28,14 +31,14 @@
 #define BISHOP_HAS_WINGS_MID (14)
 #define BISHOP_HAS_WINGS_END (32)
 
-static int PawnStackedMid[8]  = { 5,  9, 12, 13, 13, 12,  9,  5};
-static int PawnStackedEnd[8]  = {15, 19, 22, 23, 23, 22, 19, 15};
+static int PawnStackedMid[8]  = { 9, 13, 16, 17, 17, 16, 13,  9};
+static int PawnStackedEnd[8]  = {21, 25, 28, 29, 29, 28, 25, 21};
 
-static int PawnIsolatedMid[8] = {14, 20, 26, 26, 26, 26, 20, 14};
-static int PawnIsolatedEnd[8] = {10, 16, 18, 18, 18, 18, 16, 10};
+static int PawnIsolatedMid[8] = {19, 25, 31, 31, 31, 31, 25, 19};
+static int PawnIsolatedEnd[8] = {13, 19, 21, 21, 21, 21, 19, 13};
 
-static int PawnPassedMid[8]   = { 0,  0,  0,  3,  9, 18, 36,  0};
-static int PawnPassedEnd[8]   = { 0,  0,  0,  3,  9, 18, 36,  0};
+static int PawnPassedMid[8]   = { 0,  0,  0,  5, 13, 25, 46,  0};
+static int PawnPassedEnd[8]   = { 0,  0,  0,  5, 13, 25, 46,  0};
 
 static int PieceValues[8] = {PawnValue, KnightValue, BishopValue, RookValue, QueenValue, KingValue, 0, 0};
 
