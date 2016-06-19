ae7d63c24810c90a72e4dc655452ae75d12c18ae
AndyGrant <Andrew@grantnet.us>
2016-06-19 15:15:23 -0400

    Zipped up JChess, EtherealV1, and EtherealV2

--- old/_Ethereal_V1/SelfPlay/AutoPlay.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-gn = 0
-while True:
-  os.system("python SelfPlay.py " + sys.argv[1] + " " + sys.argv[2] + " True > AGame" + str(gn))
-  gn += 1
-  os.system("python SelfPlay.py " + sys.argv[2] + " " + sys.argv[1] + " True > AGame" + str(gn))
-  gn += 1
