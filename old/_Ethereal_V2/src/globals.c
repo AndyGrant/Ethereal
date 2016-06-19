4442bac0c096cc4f1051b66a7a226e54311874e7
AndyGrant <Andrew@grantnet.us>
2016-06-19 15:15:23 -0400

    Zipped up JChess, EtherealV1, and EtherealV2

--- old/_Ethereal_V2/src/globals.c
+++
@@ -1,7 +0,0 @@
-#include "types.h"
-
-board_t board;
-search_tree_t tree;
-zorbist_t zorbist;
-ttable_t table;
-evaltable_t evaltable;
--- old/_Ethereal_V2/src/zorbist.h
+++
@@ -1,8 +0,0 @@
-#ifndef ZORBIST_H
-#define ZORBIST_H
-
-void init_zorbist_t();
-int gen_bitstring();
-void init_zorbist_hash();
-
-#endif
