601af912faf5fe4ffcfc5f19016ccfad38259886
AndyGrant <Andrew@grantnet.us>
2017-01-02 02:35:35 -0500

    Release of Ethereal7.89
    Added queen mobility
    Pawns can now be both stacked and isolated
    PV Nodes are now subject to LMR, under certain conditions
    Reduced Attack weight on queen (5 => 4)
    
    Tested under the name MobStackedAndLMR (AndyGrant/TestEngines)
    Passed after 4500 Games
    WDL = (1553, 1567, 1380)
    Delta = 13.4 +- 10.15 (Z = 1.96)

--- dist/build.sh
+++ dist/build.sh
@@ -1,65 +1,39 @@
 #!/bin/sh
 
 mkdir $1
-cd $1
+mkdir $1/Win32
+mkdir $1/Win64
+mkdir $1/Android
+mkdir $1/Source
 
-mkdir Win32
-cd Win32
-mkdir Source
-cd ../
-
-mkdir Win64
-cd Win64
-mkdir Source
-cd ../
-
-mkdir Android
-cd Android
-mkdir Source
-cd ../
-
-mkdir Linux
-cd Linux
-mkdir Source
-cd ../
-
-cd ../
 make EXE=$1
-cd $1
 
-cp ../$1-Win32.exe Win32
-cp ../$1-Win64.exe Win64
-cp ../$1-Android Android
+mv $1-Win32.exe $1/Win32/$1.exe
+mv $1-Win64.exe $1/Win64/$1.exe
+mv $1-Android $1/Android/$1
 
-rm ../$1-Win32.exe
-rm ../$1-Win64.exe
-rm ../$1-Android
+cp -r ../src/* $1/Source
 
-cp -r ../../src/* Win32/Source/
-cp -r ../../src/* Win64/Source/
-cp -r ../../src/* Android/Source/
-cp -r ../../src/* Linux/Source/
-
-echo "CC = gcc"                                               >> Linux/Source/makefile
-echo "CFLAGS = -DNDEBUG -O3 -Wall -Wextra -pedantic -std=c99" >> Linux/Source/makefile
-echo "SRC = bitboards.c       \\"                             >> Linux/Source/makefile
-echo "      bitutils.c        \\"                             >> Linux/Source/makefile
-echo "      board.c           \\"                             >> Linux/Source/makefile
-echo "      castle.c          \\"                             >> Linux/Source/makefile
-echo "      evaluate.c        \\"                             >> Linux/Source/makefile
-echo "      magics.c          \\"                             >> Linux/Source/makefile
-echo "      masks.c           \\"                             >> Linux/Source/makefile
-echo "      move.c            \\"                             >> Linux/Source/makefile
-echo "      movegen.c         \\"                             >> Linux/Source/makefile
-echo "      movegentest.c     \\"                             >> Linux/Source/makefile
-echo "      psqt.c            \\"                             >> Linux/Source/makefile
-echo "      search.c          \\"                             >> Linux/Source/makefile
-echo "      time.c            \\"                             >> Linux/Source/makefile
-echo "      transposition.c   \\"                             >> Linux/Source/makefile
-echo "      uci.c             \\"                             >> Linux/Source/makefile
-echo "      zorbist.c         \\"                             >> Linux/Source/makefile
-echo ""                                                       >> Linux/Source/makefile
-echo "all:"                                                   >> Linux/Source/makefile
-echo "	\$(CC) \$(CFLAGS) \$(SRC) -o ../$1-Linux"             >> Linux/Source/makefile
+echo "CC = gcc"                                               >> $1/Source/makefile
+echo "CFLAGS = -DNDEBUG -O3 -Wall -Wextra -pedantic -std=c99" >> $1/Source/makefile
+echo "SRC = bitboards.c       \\"                             >> $1/Source/makefile
+echo "      bitutils.c        \\"                             >> $1/Source/makefile
+echo "      board.c           \\"                             >> $1/Source/makefile
+echo "      castle.c          \\"                             >> $1/Source/makefile
+echo "      evaluate.c        \\"                             >> $1/Source/makefile
+echo "      magics.c          \\"                             >> $1/Source/makefile
+echo "      masks.c           \\"                             >> $1/Source/makefile
+echo "      move.c            \\"                             >> $1/Source/makefile
+echo "      movegen.c         \\"                             >> $1/Source/makefile
+echo "      movegentest.c     \\"                             >> $1/Source/makefile
+echo "      psqt.c            \\"                             >> $1/Source/makefile
+echo "      search.c          \\"                             >> $1/Source/makefile
+echo "      time.c            \\"                             >> $1/Source/makefile
+echo "      transposition.c   \\"                             >> $1/Source/makefile
+echo "      uci.c             \\"                             >> $1/Source/makefile
+echo "      zorbist.c         \\"                             >> $1/Source/makefile
+echo ""                                                       >> $1/Source/makefile
+echo "all:"                                                   >> $1/Source/makefile
+echo "	\$(CC) \$(CFLAGS) \$(SRC) -o $1"                      >> $1/Source/makefile
 
 cd ../ 
