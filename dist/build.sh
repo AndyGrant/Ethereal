2e88880bd54605d1a7280e4a11a567f83de56a43
AndyGrant <Andrew@grantnet.us>
2017-08-01 15:07:55 -0400

    Removed 32 bit compile from build. Added wflags for GCC7.1.0

--- dist/build.sh
+++ dist/build.sh
@@ -17,23 +17,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 mkdir $1
-mkdir $1/Win32
 mkdir $1/Win64
 mkdir $1/Android
 mkdir $1/Source
 
 make EXE=$1
 
-mv $1-Win32.exe $1/Win32/$1.exe
 mv $1-Win64.exe $1/Win64/$1.exe
 mv $1-Android $1/Android/$1
 
 cp -r ../src/* $1/Source
 
-echo "CC = gcc"                                               > $1/Source/makefile
-echo "CFLAGS = -DNDEBUG -O3 -Wall -Wextra -Wshadow -std=c99" >> $1/Source/makefile
-echo "SRC = *.c"                                             >> $1/Source/makefile
-echo "all:"                                                  >> $1/Source/makefile
-echo "	\$(CC) \$(CFLAGS) \$(SRC) -o $1"                     >> $1/Source/makefile
+echo "CC = gcc"                                                                         > $1/Source/makefile
+echo "CFLAGS = -DNDEBUG -O3 -Wall -Wextra -Wshadow -Wno-implicit-fallthrough -std=c99" >> $1/Source/makefile
+echo "SRC = *.c"                                                                       >> $1/Source/makefile
+echo "all:"                                                                            >> $1/Source/makefile
+echo "	\$(CC) \$(CFLAGS) \$(SRC) -o $1"                                               >> $1/Source/makefile
 
-cd ../ 
+zip -r $1.zip $1/
+rm -r $1
