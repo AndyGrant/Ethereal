c394210dbcdfa416dbf1fa3bc98c0607d6f5c466
AndyGrant <Andrew@grantnet.us>
2017-12-16 15:01:22 -0500

    Allocate better time in X/Y case
    SPRT @ 40/10.0s Threads=1 Hash=4MB
    LLR: 3.08 (-2.94, 2.94) [0.00, 5.00]
    Games: 1150 W: 337 L: 226 D: 587
    
    BENCH : 15,651,638

--- src/uci.c
+++ src/uci.c
@@ -72,7 +72,7 @@ int main(){
         getInput(str);
         
         if (stringEquals(str, "uci")){
-            printf("id name Ethereal 8.61\n");
+            printf("id name Ethereal 8.62\n");
             printf("id author Andrew Grant\n");
             printf("option name Hash type spin default 16 min 1 max 2048\n");
             printf("option name Threads type spin default 1 min 1 max 2048\n");
