da284d93d231161ec0e1fdbafb63007a19f1c0e9
AndyGrant <Andrew@grantnet.us>
2017-12-28 17:18:48 -0500

    More aggressive capture pruning in search()
    SPRT @ 5.0+0.05s Threads=1 Hash=2MB
    LLR: 3.14 (-2.94, 2.94) [0.00, 5.00]
    Games: 23775 W: 6122 L: 5841 D: 11812
    
    SPRT @ 20.0+0.2s Threads=1 Hash=8MB
    LLR: 3.04 (-2.94, 2.94) [0.00, 5.00]
    Games: 6925 W: 1533 L: 1394 D: 3998
    
    BENCH : 10,325,461

--- src/search.c
+++ src/search.c
@@ -494,14 +494,24 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (    !PvNode
             &&  !isQuiet
             &&   played >= 1
-            &&   depth <= 3
+            &&   depth <= 4
+            &&   MoveType(currentMove) != ENPASS_MOVE
             &&   MoveType(currentMove) != PROMOTION_MOVE
             &&  !ei.positionIsDrawn
-            &&  (ei.attacked[!board->turn]   & (1ull << MoveTo(currentMove)))
             && !(ei.attackedBy2[board->turn] & (1ull << MoveTo(currentMove)))
-            &&  PieceValues[PieceType(board->squares[MoveTo  (currentMove)])][MG]
-             <  PieceValues[PieceType(board->squares[MoveFrom(currentMove)])][MG])
-            continue;
+            &&   PieceValues[PieceType(board->squares[MoveTo  (currentMove)])][MG]
+             <   PieceValues[PieceType(board->squares[MoveFrom(currentMove)])][MG]){
+                 
+          
+            // If the target piece has two or more defenders, we will prune up to depth 4
+            if (ei.attackedBy2[!board->turn] & (1ull << MoveTo(currentMove)))
+                continue;
+            
+            // Otherwise, if the piece has one defender, we will prune up to depth 3
+            if (    depth <= 3
+                && (ei.attacked[!board->turn] & (1ull << MoveTo(currentMove))))
+                continue;
+         }
         
         // Apply and validate move before searching
         applyMove(board, currentMove, undo);
