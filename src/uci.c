c58c566d7a2ed8714e71da073d1177e895116675
AndyGrant <Andrew@grantnet.us>
2018-01-19 06:48:11 -0500

    Attempt to resolve playing of illegal moves
    Every so often I'll see cutechess terminate a game because of an illegal move being played, however I cannot track it down. There are 2 thoughts as to how this happens; 1. The search time expires before completing the depth one search and therefore there is not guaranteed to be a move in the PV, 2. There is some issue with draw detection / mate distance pruning which causes searches to return without a PV. Therefore in root nodes, even though it is not expected, we will ignore the fact that the position is drawn and search anyway.
    
    Regression testing showed a large elo gain, but I assume that to be noise.
    
    Testing the conditions to avoid pruning
    SPRT @ 5.0+0.05s Threads=1 Hash=2MB
    LLR: 2.98 (-2.94, 2.94) [-5.00, 0.00]
    Games: 16050 W: 3929 L: 3943 D: 8178
    
    Testing the conditions to avoid early search terminatoin
    SPRT @ 5.0+0.05s Threads=1 Hash=2MB
    LLR: 3.07 (-2.94, 2.94) [-5.00, 0.00]
    Games: 2600 W: 696 L: 608 D: 1296
    
    Testing the conditions to avoid early search termination
    SPRT @ 20.0+0.2s Threads=1 Hash=8MB
    LLR: 2.99 (-2.94, 2.94) [-5.00, 0.00]
    Games: 1500 W: 345 L: 271 D: 884
    
    BENCH : 10,016,803

--- src/uci.c
+++ src/uci.c
@@ -69,7 +69,7 @@ int main(){
         getInput(str);
         
         if (stringEquals(str, "uci")){
-            printf("id name Ethereal 8.86\n");
+            printf("id name Ethereal 8.87\n");
             printf("id author Andrew Grant\n");
             printf("option name Hash type spin default 16 min 1 max 65536\n");
             printf("option name Threads type spin default 1 min 1 max 2048\n");
