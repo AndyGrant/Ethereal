5cf82498ccd4a43e0e7666267ffd9950e6c9d3db
laldon <36208136+laldon@users.noreply.github.com>
2018-02-20 20:38:34 +0100

    Allow evaluation to better predict search results, thus allowing for more aggressive aspiration windows
    Tuned using Jeffery's position set, searched to depth 9. This should allow the evaluation to more closely match short searches, which allows us to use a new type of aspiration window.
    
    SPRT @ 5.0+0.05s Threads=1 Hash=2MB
    LLR: -0.66 (-2.94, 2.94) [0.00, 5.00]
    Games: 4000 W: 986 L: 995 D: 2019
    
    SPRT @ 20.0+0.2s Threads=1 Hash=8MB
    LLR: 3.02 (-2.94, 2.94) [0.00, 5.00]
    Games: 4200 W: 960 L: 807 D: 2433
    
    SPRT @ 60.0+0.6s Threads=1 Hash=32MB
    LLR: 2.98 (-2.94, 2.94) [0.00, 5.00]
    Games: 4875 W: 1017 L: 867 D: 2991
    
    BENCH : 11,402,028

--- src/search.c
+++ src/search.c
@@ -248,14 +248,14 @@ int aspirationWindow(Thread* thread, int depth){
     if (depth > 4 && abs(values[mainDepth-1]) < MATE / 2){
         
         // Dynamically compute the upper margin based on previous scores
-        upper = MAX(    4,  1.6 * (values[mainDepth-1] - values[mainDepth-2]));
-        upper = MAX(upper,  2.0 * (values[mainDepth-2] - values[mainDepth-3]));
-        upper = MAX(upper,  0.8 * (values[mainDepth-3] - values[mainDepth-4]));
+        upper = MAX(    8,  1.6 * (values[mainDepth-1] - values[mainDepth-2]));
+        upper = MAX(upper,  1.3 * (values[mainDepth-2] - values[mainDepth-3]));
+        upper = MAX(upper,  1.0 * (values[mainDepth-3] - values[mainDepth-4]));
         
         // Dynamically compute the lower margin based on previous scores
-        lower = MAX(    4, -1.6 * (values[mainDepth-1] - values[mainDepth-2]));
-        lower = MAX(lower, -2.0 * (values[mainDepth-2] - values[mainDepth-3]));
-        lower = MAX(lower, -0.8 * (values[mainDepth-3] - values[mainDepth-4])); 
+        lower = MAX(    8, -1.6 * (values[mainDepth-1] - values[mainDepth-2]));
+        lower = MAX(lower, -1.3 * (values[mainDepth-2] - values[mainDepth-3]));
+        lower = MAX(lower, -1.0 * (values[mainDepth-3] - values[mainDepth-4])); 
         
         // Create the aspiration window
         alpha = values[mainDepth-1] - lower;
