113578863292c4c73eb070902e3bef0d3ef3c270
AndyGrant <Andrew@grantnet.us>
2018-03-05 15:44:34 -0500

    Tweak pruning values
    SPRT @ 5.0+0.05s Threads=1 Hash=2MB
    LLR: 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games: 4050 W: 1107 L: 947 D: 1996
    
    SPRT @ 20.0+0.2s Threads=1 Hash=8MB
    LLR: 1.28 (-2.94, 2.94) [0.00, 5.00]
    Games: 7625 W: 1626 L: 1534 D: 4465
    
    SPRT @ 60.0+0.6s Threads=1 Hash=32MB
    LLR: 1.97 (-2.94, 2.94) [0.00, 5.00]
    Games: 7875 W: 1512 L: 1394 D: 4969
    
    BENCH : 10,969,182

--- src/search.c
+++ src/search.c
@@ -457,7 +457,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         &&  hasNonPawnMaterial(board, board->turn)
         &&  board->history[board->numMoves-1] != NULL_MOVE){
             
-        R = 4 + depth / 6 + (eval - beta + 312) / 625; 
+        R = 4 + depth / 6 + (eval - beta + 200) / 400;
             
         applyNullMove(board, undo);
         
@@ -606,7 +606,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             &&  isQuiet){
             
             // Baseline R based on number of moves played and current depth
-            R = 2 + (played - 4) / 8 + (depth - 4) / 6;
+            R = 2 + (played - 4) / 8 + (depth - 6) / 4;
             
             // Increase R by an additional two ply for non PvNodes
             R += 2 * !PvNode;
