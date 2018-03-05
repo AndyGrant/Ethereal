ebb688cfab68ec133caaea26e10848bf372c779b
AndyGrant <Andrew@grantnet.us>
2018-03-05 15:44:34 -0500

    Tweak pruning values
    SPRT @ 5.0+0.05s Threads=1 Hash=2MB
    LLR: 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games: 4050 W: 1107 L: 947 D: 1996
    
    SPRT @ 20.0+0.2s Threads=1 Hash=8MB
    LLR: 1.28 (-2.94, 2.94) [0.00, 5.00]
    Games: 7625 W: 1626 L: 1534 D: 4465
    
    SPRT @ 60.0+0.6s Threads=1 Hash=32MB
    LLR: 1.97 (-2.94, 2.94) [0.00, 5.00]
    Games: 7875 W: 1512 L: 1394 D: 4969
    
    BENCH : 10,969,182

--- src/search.h
+++ src/search.h
@@ -71,11 +71,11 @@ int bestTacticalMoveValue(Board* board);
 
 static const int RazorDepth = 4;
 
-static const int RazorMargins[] = {0, 700, 750, 810, 900};
+static const int RazorMargins[] = {0, 300, 350, 410, 500};
 
 static const int BetaPruningDepth = 8;
 
-static const int FutilityMargin = 110;
+static const int FutilityMargin = 85;
 
 static const int ProbCutDepth = 5;
 
@@ -91,6 +91,6 @@ static const int LateMovePruningDepth = 8;
 
 static const int LateMovePruningCounts[] = {0, 5, 7, 11, 16, 24, 33, 43, 56};
 
-static const int QFutilityMargin = 86;
+static const int QFutilityMargin = 100;
 
 #endif
