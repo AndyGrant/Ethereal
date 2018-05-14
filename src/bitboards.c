b40612a2859aa4e196012ef21d6ea28c39c6bec6
lucasart <lucas.braesch@gmail.com>
2018-05-14 09:21:30 +0800

    Bitboard code-style massage (#33)
    No functional change.
    
    BENCH : 8,271,334

--- src/bitboards.c
+++ src/bitboards.c
@@ -1,26 +1,35 @@
 /*
   Ethereal is a UCI chess playing engine authored by Andrew Grant.
   <https://github.com/AndyGrant/Ethereal>     <andrew@grantnet.us>
-  
+
   Ethereal is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   Ethereal is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <assert.h>
 #include <stdint.h>
 
 #include "bitboards.h"
-#include "types.h"
 
 const uint64_t Files[FILE_NB] = {FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H};
-
 const uint64_t Ranks[RANK_NB] = {RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8};
+
+int File(int sq) {
+    assert(0 <= sq && sq < SQUARE_NB);
+    return sq % FILE_NB;
+}
+
+int Rank(int sq) {
+    assert(0 <= sq && sq < SQUARE_NB);
+    return sq / FILE_NB;
+}
--- src/bitboards.h
+++ src/bitboards.h
@@ -1,58 +1,57 @@
 /*
   Ethereal is a UCI chess playing engine authored by Andrew Grant.
   <https://github.com/AndyGrant/Ethereal>     <andrew@grantnet.us>
-  
+
   Ethereal is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   Ethereal is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _BITBOARDS_H
-#define _BITBOARDS_H
+#pragma once
 
 #include <stdint.h>
 
 #include "types.h"
 
-#define RANK_8 (0xFF00000000000000ull)
-#define RANK_7 (0x00FF000000000000ull)
-#define RANK_6 (0x0000FF0000000000ull)
-#define RANK_5 (0x000000FF00000000ull)
-#define RANK_4 (0x00000000FF000000ull)
-#define RANK_3 (0x0000000000FF0000ull)
-#define RANK_2 (0x000000000000FF00ull)
-#define RANK_1 (0x00000000000000FFull)
+enum {
+    RANK_8 = 0xFF00000000000000ull,
+    RANK_7 = 0x00FF000000000000ull,
+    RANK_6 = 0x0000FF0000000000ull,
+    RANK_5 = 0x000000FF00000000ull,
+    RANK_4 = 0x00000000FF000000ull,
+    RANK_3 = 0x0000000000FF0000ull,
+    RANK_2 = 0x000000000000FF00ull,
+    RANK_1 = 0x00000000000000FFull,
 
-#define FILE_A (0x0101010101010101ull)
-#define FILE_B (0x0202020202020202ull)
-#define FILE_C (0x0404040404040404ull)
-#define FILE_D (0x0808080808080808ull)
-#define FILE_E (0x1010101010101010ull)
-#define FILE_F (0x2020202020202020ull)
-#define FILE_G (0x4040404040404040ull)
-#define FILE_H (0x8080808080808080ull)
+    FILE_A = 0x0101010101010101ull,
+    FILE_B = 0x0202020202020202ull,
+    FILE_C = 0x0404040404040404ull,
+    FILE_D = 0x0808080808080808ull,
+    FILE_E = 0x1010101010101010ull,
+    FILE_F = 0x2020202020202020ull,
+    FILE_G = 0x4040404040404040ull,
+    FILE_H = 0x8080808080808080ull,
 
-#define LEFT_WING  (FILE_A | FILE_B | FILE_C)
-#define RIGHT_WING (FILE_F | FILE_G | FILE_H)
+    LEFT_WING = FILE_A | FILE_B | FILE_C,
+    RIGHT_WING = FILE_F | FILE_G | FILE_H,
 
-#define WHITE_SQUARES (0x55AA55AA55AA55AAull)
-#define BLACK_SQUARES (0xAA55AA55AA55AA55ull)
+    WHITE_SQUARES = 0x55AA55AA55AA55AAull,
+    BLACK_SQUARES = 0xAA55AA55AA55AA55ull,
 
-#define PROMOTION_RANKS (RANK_1 | RANK_8)
+    PROMOTION_RANKS = RANK_1 | RANK_8
+};
 
 extern const uint64_t Files[FILE_NB];
 extern const uint64_t Ranks[RANK_NB];
 
-#define File(sq) ((sq)  & 7)
-#define Rank(sq) ((sq) >> 3)
-
-#endif
+int File(int sq);
+int Rank(int sq);
