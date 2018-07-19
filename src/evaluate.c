e23966908240ed232e84a250b236ee73496cf36f
AndyGrant <Andrew@grantnet.us>
2018-07-18 21:13:11 -0400

    Adjust base King Safety, and NoQueen component
    ELO   | 2.06 +- 1.56 (95%)
    SPRT  | 10.0+0.1s Threads=1 Hash=8MB
    LLR   | 3.00 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 81975 W: 17904 L: 17419 D: 46652
    
    ELO   | 2.23 +- 1.80 (95%)
    SPRT  | 60.0+0.6s Threads=1 Hash=64MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 48050 W: 8223 L: 7914 D: 31913
    
    BENCH : 5,930,849

--- src/evaluate.c
+++ src/evaluate.c
@@ -160,12 +160,12 @@ const int KSAttackWeight[]  = { 0, 16, 6, 10, 8, 0 };
 const int KSAttackValue     =   44;
 const int KSWeakSquares     =   38;
 const int KSFriendlyPawns   =  -22;
-const int KSNoEnemyQueens   = -256;
+const int KSNoEnemyQueens   = -276;
 const int KSSafeQueenCheck  =   95;
 const int KSSafeRookCheck   =   94;
 const int KSSafeBishopCheck =   51;
 const int KSSafeKnightCheck =  123;
-const int KSAdjustment      =  -38;
+const int KSAdjustment      =  -18;
 
 /* Passed Pawn Evaluation Terms */
 
--- src/uci.h
+++ src/uci.h
@@ -21,7 +21,7 @@
 
 #include "types.h"
 
-#define VERSION_ID "10.58"
+#define VERSION_ID "10.59"
 
 #if defined(USE_PEXT)
     #define ETHEREAL_VERSION VERSION_ID" (PEXT)"
