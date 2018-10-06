2ad99ce06958de2eea7ff3c26d5586849fd82b79
lucasart <lucas.braesch@gmail.com>
2018-10-07 07:53:13 +0800

    6279265

--- src/evaluate.c
+++ src/evaluate.c
@@ -226,9 +226,9 @@ const int PassedPawn[2][2][8] = {
     S(  -1, 101), S(  44, 223), S( 125, 381), S(   0,   0)}},
 };
 
-const int PassedFriendlyDistance = S(   4,  -6);
+const int PassedFriendlyDistance[] = {0, 0, 0, S(0,-3), S(0,-13), S(0,-24), S(0,-33)};
 
-const int PassedEnemyDistance = S(  -1,   8);
+const int PassedEnemyDistance[] = {0, 0, 0, S(0,6), S(0,27), S(0,48), S(0,67)};
 
 const int PassedSafePromotionPath = S(   0,  26);
 
@@ -760,12 +760,12 @@ int evaluatePassedPawns(EvalInfo* ei, Board* board, int colour){
 
         // Evaluate based on distance from our king
         dist = distanceBetween(sq, ei->kingSquare[US]);
-        eval += dist * PassedFriendlyDistance;
+        eval += dist * PassedFriendlyDistance[rank];
         if (TRACE) T.PassedFriendlyDistance[US] += dist;
 
         // Evaluate based on distance from their king
         dist = distanceBetween(sq, ei->kingSquare[THEM]);
-        eval += dist * PassedEnemyDistance;
+        eval += dist * PassedEnemyDistance[rank];
         if (TRACE) T.PassedEnemyDistance[US] += dist;
 
         // Apply a bonus when the path to promoting is uncontested
