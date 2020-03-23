98cc6589ce64ec3362fe4c7e641443f5b06c6085
AndyGrant <Andrew@grantnet.us>
2020-03-23 12:21:00 -0400

    Increase LMR for candidate-extension moves
    ELO   | 4.77 +- 3.69 (95%)
    SPRT  | 12.0+0.12s Threads=1 Hash=8MB
    LLR   | 3.04 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 16525 W: 4131 L: 3904 D: 8490
    http://chess.grantnet.us/test/4933/
    
    ELO   | 2.36 +- 1.80 (95%)
    SPRT  | 60.0+0.6s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 55770 W: 11044 L: 10665 D: 34061
    http://chess.grantnet.us/test/4937/
    
    BENCH : 5,508,011

--- src/search.c
+++ src/search.c
@@ -460,15 +460,40 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
         if (RootNode && !thread->index && elapsedTime(thread->info) > CurrmoveTimerMS)
             uciReportCurrentMove(board, move, played + thread->multiPV, depth);
 
-        // Step 13 (~249 elo). Late Move Reductions. Compute the reduction,
+        // Identify moves which are candidate singular moves
+        singular =  !RootNode
+                 &&  depth >= 8
+                 &&  move == ttMove
+                 &&  ttDepth >= depth - 2
+                 && (ttBound & BOUND_LOWER);
+
+        // Step 13 (~60 elo). Extensions. Search an additional ply when the move comes from the
+        // Transposition Table and appears to beat all other moves by a fair margin. Otherwise,
+        // extend moves which were not candidates for singularity, but are for positions that
+        // are in check, as well as moves which have excellent continuation history scores
+
+        extension = singular
+                  ? moveIsSingular(thread, ttMove, ttValue, depth, height, beta, &multiCut)
+                  : inCheck || (isQuiet && quietsSeen <= 4 && cmhist >= 10000 && fmhist >= 10000);
+
+        // Step 14. MultiCut. Sometimes candidate Singular moves are shown to be non-Singular.
+        // If this happens, and the rBeta used for that proof is greater than beta, then we
+        // have multiple moves which appear to beat beta at a reduced depth.
+
+        if (singular && multiCut) {
+            revert(thread, board, move, height);
+            return MAX(ttValue - depth, -MATE);
+        }
+
+        // Step 15 (~249 elo). Late Move Reductions. Compute the reduction,
         // allow the later steps to perform the reduced searches
         if (isQuiet && depth > 2 && played > 1) {
 
             /// Use the LMR Formula as a starting point
             R  = LMRTable[MIN(depth, 63)][MIN(played, 63)];
 
-            // Increase for non PV and non improving nodes
-            R += !PvNode + !improving;
+            // Increase for non PV, non improving, and extended nodes
+            R += !PvNode + !improving + extension;
 
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
@@ -483,31 +508,6 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
             R  = MIN(depth - 1, MAX(R, 1));
 
         } else R = 1;
-
-        // Identify moves which are candidate singular moves
-        singular =  !RootNode
-                 &&  depth >= 8
-                 &&  move == ttMove
-                 &&  ttDepth >= depth - 2
-                 && (ttBound & BOUND_LOWER);
-
-        // Step 14 (~60 elo). Extensions. Search an additional ply when the move comes from the
-        // Transposition Table and appears to beat all other moves by a fair margin. Otherwise,
-        // extend moves which were not candidates for singularity, but are for positions that
-        // are in check, as well as moves which have excellent continuation history scores
-
-        extension = singular
-                  ? moveIsSingular(thread, ttMove, ttValue, depth, height, beta, &multiCut)
-                  : inCheck || (isQuiet && quietsSeen <= 4 && cmhist >= 10000 && fmhist >= 10000);
-
-        // Step 15. MultiCut. Sometimes candidate Singular moves are shown to be non-Singular.
-        // If this happens, and the rBeta used for that proof is greater than beta, then we
-        // have multiple moves which appear to beat beta at a reduced depth.
-
-        if (singular && multiCut) {
-            revert(thread, board, move, height);
-            return MAX(ttValue - depth, -MATE);
-        }
 
         // Factor the extension into the new depth. Do not extend at the root
         newDepth = depth + (extension && !RootNode);
--- src/uci.h
+++ src/uci.h
@@ -22,7 +22,7 @@
 
 #include "types.h"
 
-#define VERSION_ID "12.02"
+#define VERSION_ID "12.03"
 
 #if defined(USE_PEXT)
     #define ETHEREAL_VERSION VERSION_ID" (PEXT)"
