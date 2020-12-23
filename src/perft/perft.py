8f952ee13081fe5ef7d8d955b137efc122ea1a57
Andrew Grant <andrew@grantnet.us>
2020-12-23 02:28:36 -0500

    Small adjustments to the python script for PERFT
    NO FUNCTIONAL CHANGE
    
    BENCH : 4,336,186

--- src/perft/perft.py
+++ src/perft/perft.py
@@ -1,66 +1,74 @@
-# Ethereal is a UCI chess playing engine authored by Andrew Grant.
-# <https://github.com/AndyGrant/Ethereal>     <andrew@grantnet.us>
-#
-# Ethereal is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ethereal is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#   Ethereal is a UCI chess playing engine authored by Andrew Grant.          #
+#   <https://github.com/AndyGrant/Ethereal>     <andrew@grantnet.us>          #
+#                                                                             #
+#   Ethereal is free software: you can redistribute it and/or modify          #
+#   it under the terms of the GNU General Public License as published by      #
+#   the Free Software Foundation, either version 3 of the License, or         #
+#   (at your option) any later version.                                       #
+#                                                                             #
+#   Ethereal is distributed in the hope that it will be useful,               #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU General Public License for more details.                              #
+#                                                                             #
+#   You should have received a copy of the GNU General Public License         #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 import argparse, subprocess, sys, time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("ENGINE", help="Path to Engine")
-parser.add_argument("DATASET", help="PERFT data file")
+parser.add_argument('engine',  help='Path to Engine')
+parser.add_argument('dataset', help='PERFT data file')
+parser.add_argument('--depth', help='Depth of PERFT [ blank = unlimited ]', default=128)
 arguments = parser.parse_args()
 
 process = subprocess.Popen(
-    arguments.ENGINE,
+    arguments.engine,
     stderr=subprocess.STDOUT,
     stdout=subprocess.PIPE,
     stdin=subprocess.PIPE,
     universal_newlines=True
 )
 
-with open(arguments.DATASET) as fin:
+with open(arguments.dataset) as fin:
     data = fin.readlines()
 
-print("---------------------------------------------------------------------------------------------------------------")
-print("| Depth |    Nodes    |                                      FEN                                     | STATUS |")
-print("---------------------------------------------------------------------------------------------------------------")
+SEPERATOR = '-' * 111 + '\n'
+HEADER    = '| {0:^5} | {1:^11} | {2:^76} | {3:^6} |\n'
+
+sys.stdout.write(SEPERATOR)
+sys.stdout.write(HEADER.format('Depth', 'Nodes', 'FEN', 'Status'))
+sys.stdout.write(SEPERATOR)
 
 start = time.time()
 
 for line in data:
-    tokens = line.split(";")
-    fen, last = tokens[0], tokens[-1]
-    depth = int(last.split(" ")[0][1:])
-    nodes = int(last.split(" ")[1])
 
-    sys.stdout.write("| {0:>5} | {1:>11} | {2:<76} | ".format(depth, nodes, fen)),
-    sys.stdout.flush()
+    for token in line.split(';')[1:]:
 
-    process.stdin.write("position fen %s\n" % (fen))
-    process.stdin.write("perft %d\n" % (depth))
-    process.stdin.flush()
+        fen   = line.split(';')[0]
+        depth = int(token.split(' ')[0][1:])
+        nodes = int(token.split(' ')[1])
+        if depth > arguments.depth: break
 
-    found = int(process.stdout.readline().strip())
+        sys.stdout.write('| {0:>5} | {1:>11} | {2:<76} | '.format(depth, nodes, fen))
+        sys.stdout.flush()
 
-    if found == nodes: sys.stdout.write (" PASS  |\n")
-    if found != nodes: sys.stdout.write (" FAIL  | (%10d)\n" % (found))
+        process.stdin.write('position fen %s\n' % (fen))
+        process.stdin.write('perft %d\n' % (depth))
+        process.stdin.flush()
 
-end = time.time()
+        found = int(process.stdout.readline().strip())
 
-print("---------------------------------------------------------------------------------------------------------------")
+        if found == nodes: sys.stdout.write (' PASS  |\n')
+        if found != nodes: sys.stdout.write (' FAIL  | (%10d)\n' % (found))
 
-process.stdin.write("quit")
+process.stdin.write('quit')
 process.stdin.flush()
 
-print("Total Time: %dms" % (1000 * (end - start)))
+sys.stdout.write(SEPERATOR)
+sys.stdout.write('Total Time: %dms' % (1000 * (time.time() - start)))
--- src/uci.h
+++ src/uci.h
@@ -22,7 +22,7 @@
 
 #include "types.h"
 
-#define VERSION_ID "12.78"
+#define VERSION_ID "12.79"
 
 #if defined(USE_PEXT)
     #define ETHEREAL_VERSION VERSION_ID" (PEXT)"
