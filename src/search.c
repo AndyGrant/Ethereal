b400ee7bb09d4e83350df9838a15f1e45be96463
AT <manyoso@users.noreply.github.com>
2021-01-28 11:54:29 -0500

    Make LMP a bit more aggressive by removing rounding (#173)
    ELO   | -1.37 +- 3.33 (95%)
    SPRT  | 10.0+0.1s Threads=1 Hash=8MB
    LLR   | -3.01 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 15456 W: 2836 L: 2897 D: 9723
    
    ELO   | 2.52 +- 2.01 (95%)
    SPRT  | 60.0+0.6s Threads=1 Hash=64MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 28784 W: 3727 L: 3518 D: 21539
    
    ELO   | 2.90 +- 2.24 (95%)
    SPRT  | 60.0+0.6s Threads=1 Hash=64MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 21940 W: 2711 L: 2528 D: 16701
    
    BENCH : 4,224,732

--- src/search.c
+++ src/search.c
@@ -59,8 +59,8 @@ void initSearch() {
             LMRTable[depth][played] = 0.75 + log(depth) * log(played) / 2.25;
 
     for (int depth = 1; depth < 9; depth++) {
-        LateMovePruningCounts[0][depth] = roundf(2.5 + 2 * depth * depth / 4.5);
-        LateMovePruningCounts[1][depth] = roundf(4.0 + 4 * depth * depth / 4.5);
+        LateMovePruningCounts[0][depth] = 2.5 + 2 * depth * depth / 4.5;
+        LateMovePruningCounts[1][depth] = 4.0 + 4 * depth * depth / 4.5;
     }
 }
 
--- src/uci.h
+++ src/uci.h
@@ -22,7 +22,7 @@
 
 #include "types.h"
 
-#define VERSION_ID "12.88"
+#define VERSION_ID "12.89"
 
 #if defined(USE_PEXT)
     #define ETHEREAL_VERSION VERSION_ID" (PEXT)"
